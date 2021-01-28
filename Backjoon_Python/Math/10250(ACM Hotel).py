781eaf25fafe8ddc37d4ecf055f20d65175649dd
khsung <khsung0@nate.com>
2021-01-28 12:35:07 +0900

    ACM 호텔(합격)

--- Backjoon_Python/Math/10250(ACM Hotel).py
+++ Backjoon_Python/Math/10250(ACM Hotel).py
@@ -1,6 +1,16 @@
 
+#10250 ACM 호텔
 testcase=int(input())
+res=[]
 
 for i in range(testcase):
     h,w,n=input().split(" ")
-    print(h,w,n)
+    if int(n)%int(h)==0:
+        x=int(int(n)/int(h))
+        y=int(h)
+    else:
+        x=int(int(n)/int(h))+1
+        y=int(n)%int(h)
+    res.append(100*y+x)
+for i in range(len(res)):
+    print(res[i])
