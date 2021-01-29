d2049c4deb417feabc221fc4f48fef600a269c9d
khsung <khsung0@nate.com>
2021-01-29 17:00:09 +0900

    스택 수열(합격)

--- Backjoon_Python/Stack/1874(Stack Sequence).py
+++ Backjoon_Python/Stack/1874(Stack Sequence).py
@@ -1,6 +1,22 @@
 #1874 Stack Sequence
 sequence=[]
+stack=[]
+op=[]
 num=int(input())
 for i in range(num):
     x=int(input())
     sequence.append(x)
+
+for i in range(1,max(sequence)+1):
+    stack.append(i)
+    op.append('+')
+    while len(stack)>0 and sequence[0]==stack[-1]:
+        sequence.pop(0)
+        stack.pop()
+        op.append('-')
+if len(stack)==0:
+    for i in range(len(op)):
+        print(op[i])
+else:
+    print("NO")
+
