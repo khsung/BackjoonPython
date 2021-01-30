a766c5b71df42b32c64f35b87c94bcbe76f446cf
khsung <khsung0@nate.com>
2021-01-30 19:16:56 +0900

    수 정렬하기2

--- Backjoon_Python/Sort/2751(Sort Number2).py
+++ Backjoon_Python/Sort/2751(Sort Number2).py
@@ -1,15 +1,8 @@
 
 #2751 Sort Number2
-import heapq
 num=int(input())
 numbers=[]
 for i in range(num):
     numbers.append(int(input()))
-heap=[]
-sorted_num=[]
-for i in numbers:
-    heapq.heappush(heap,i)
-while heap:
-    sorted_num.append(heapq.heappop(heap))
-for i in range(len(sorted_num)):
-    print(sorted_num[i])
+for i in sorted(numbers):
+    print(i)
