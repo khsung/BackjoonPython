1bf019a565d931c10723e5468d23732295935427
khsung <khsung0@nate.com>
2021-01-31 19:33:20 +0900

    좌표 정렬하기(합격)

---
+++ Backjoon_Python/Sort/11650(Location Sort).py
@@ -0,0 +1,10 @@
+#11650 Location Sort
+
+num = int(input())
+location=[]
+for i in range(num):
+    a, b=input().split()
+    location.append([int(a),int(b)])
+location.sort()
+for i in range(len(location)):
+    print(location[i][0],location[i][1])
