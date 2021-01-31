7a4070a8883f51549931a5ed6d35aeab3488419f
khsung <khsung0@nate.com>
2021-02-01 00:02:13 +0900

    덩치(합격)

--- Backjoon_Python/BruteForce/7568(Bulk).py
+++ Backjoon_Python/BruteForce/7568(Bulk).py
@@ -1,8 +1,16 @@
 #7568 Bulk
 num=int(input())
 array=[]
+rankarray=[0 for i in range(num)]
 for i in range(num):
     a,b=input().split()
-    array.append([a,b])
-array.sort()
-print(array)
+    array.append([int(a),int(b)])
+
+for i in range(len(array)):
+    count=0
+    for j in range(len(array)):
+        if array[i][0]<array[j][0] and array[i][1]<array[j][1]:
+            count+=1
+    rankarray[i]=count+1
+for i in range(len(rankarray)):
+    print(rankarray[i],end=" ")
