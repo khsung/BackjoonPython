fef15a0200bfa47f8d795598370a202cc016949e
khsung <khsung0@nate.com>
2021-02-01 00:39:32 +0900

    통계학(시간초과)

--- Backjoon_Python/Sort/2108(Statistics).py
+++ Backjoon_Python/Sort/2108(Statistics).py
@@ -1,18 +1,17 @@
 #2108 통계학
-
+import sys
+from collections import Counter
 num=int(input())
 max_num=-4001
 min_num=4001
 sum1=0
 numbers=[]
 first=0
-count1=[0 for i in range(8001)]
 many=0
 for i in range(num):
-    a=int(input())
+    a=int(sys.stdin.readline())
     numbers.append(a)
     sum1+=a
-    count1[a+4000]+=1
     if min_num>a:
         min_num=a
     if max_num<a:
@@ -21,11 +20,9 @@ numbers.sort()
 avg=round(sum1/num)
 print(avg)
 print(numbers[int(num/2)])
-temp1=max(count1)
-temp2=count1.index(max(count1))
-count1[temp2]=0
-if temp1==max(count1) and first == 0:
-    first=1
-    temp2=count1.index(max(count1))
-print(temp2-4000)
+temp=Counter(numbers).most_common()
+if len(temp)>1:
+    print(temp[1][0])
+else:
+    print(temp[0][0])
 print(max_num-min_num)
