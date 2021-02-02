ad531aab558d5d5dda36a93777a48a6f28f7f0f3
khsung <khsung0@nate.com>
2021-02-02 11:10:09 +0900

    숫자 카드2(메모리 초과)

--- Backjoon_Python/BinarySearch/10816(Number Card).py
+++ Backjoon_Python/BinarySearch/10816(Number Card).py
@@ -1,7 +1,22 @@
 #10816 숫자 카드2
+import sys
+sys.setrecursionlimit(10**6)
+def binary(array1,number,left,right):
+    mid=(left+right)//2
+    if array1[mid]==number:
+        return array1.count(number)
+    elif left==right:
+        return 0
+    else:
+        if array1[mid]>number:
+            return binary(array1,number,left,mid-1)
+        else:
+            return binary(array1,number,mid+1,right)
+
 num1=int(input())
 array1=list(map(int,input().split()))
 num2=int(input())
 array2=list(map(int,input().split()))
+array1.sort()
 for i in range(len(array2)):
-    print(array1.count(array2[i]),end=" ")
+    print(binary(array1,array2[i],0,len(array1)),end=" ")
