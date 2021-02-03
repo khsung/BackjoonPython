bc00445e5c67fdbfff58e6798fcf80333b9affb2
khsung <khsung0@nate.com>
2021-02-03 12:44:16 +0900

    주유소(합격)

--- Backjoon_Python/GreedyAlgorithm/13305(Gas Station).py
+++ Backjoon_Python/GreedyAlgorithm/13305(Gas Station).py
@@ -5,15 +5,20 @@ path = list(map(int,path))
 station = input().split()
 station = list(map(int,station))
 station.pop()
-right = len(station) - 1
+end = len(station) - 1
 costsum = 0
-while right > -1:
-    path_sum = 0
-    mincost = min(station)
-    mincostindex = station.index(mincost)
-    for i in range(mincostindex,right + 1):
-        path_sum+=path[i]
-    costsum+=path_sum * mincost
-    station = station[:mincostindex]
-    right = mincostindex - 1
+path_sum = 0
+mincost = min(station)
+mincostindex = station.index(mincost)
+for i in range(mincostindex,len(station)):
+    path_sum+=path[i]
+costsum+=mincost * path_sum
+for i in range(mincostindex):
+    if i == 0:
+        temp = station[i]
+    if temp > station[i + 1]:
+        costsum=costsum+(temp * path[i])
+        temp = station[i + 1]
+    else:
+        costsum=costsum+(temp * path[i])
 print(costsum)
