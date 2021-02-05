52244f5ab36efc082736ea40823e7e3cd8de6fd0
khsung <khsung0@nate.com>
2021-02-05 10:15:49 +0900

    색종이 만들기(메모리 초과)

--- Backjoon_Python/DivideAndConquer/2630(Making Colored paper).py
+++ Backjoon_Python/DivideAndConquer/2630(Making Colored paper).py
@@ -1,23 +1,23 @@
 #2630 색종이 만들기
+
+
 color = [0,0]
 square = []
-def findsquare(rleft, rright,cleft, cright):  #4개로 구분지어 받아야됨
-    temp = []
-    wcount = 0
-    bcount = 0
-    for i in range(left, right + 1):
-        temp.append(square[i][left:right + 1])
-        wcount+=square[i][left:right + 1].count(0)
-        bcount+=square[i][left:right + 1].count(1)
-    print(temp)
-    if bcount == 0:
-        color[0]+=1
-    elif wcount == 0:
-        color[1]+=1
-    else:
-        mid = (left + right) // 2
-        findsquare(left, mid)
-        findsquare(mid + 1, right)
+def findsquare(rleft, rright,cleft, cright):
+    currcolor=square[rleft][cleft]
+    check=0
+    for i in range(rleft, rright + 1):
+        for j in range(cleft, cright+1):
+            if square[i][j]!=currcolor:
+                mid = (rleft + rright) // 2
+                findsquare(rleft, mid,cleft,mid)
+                findsquare(rleft, mid,mid + 1, cright)
+                findsquare(mid + 1, rright,cleft,mid)
+                findsquare(mid + 1, rright,mid + 1, cright)
+                check=1
+                return
+    if check==0:
+        color[currcolor]+=1
 num = int(input())
 for i in range(num):
     temp = input().split()
@@ -25,6 +25,8 @@ for i in range(num):
 findsquare(0, num - 1,0, num - 1)
 print(color[0])
 print(color[1])
+
+
 
 #white = 0
 #blue = 0
