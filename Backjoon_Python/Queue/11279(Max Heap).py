0eddd304605f437f41124f2b9913ebbad7b0d356
khsung <khsung0@nate.com>
2021-02-06 13:26:08 +0900

    최대 힙

---
+++ Backjoon_Python/Queue/11279(Max Heap).py
@@ -0,0 +1,15 @@
+#11279 최대 힙
+import heapq
+import sys
+num=int(input())
+array=[]
+res=[]
+for i in range(num):
+    a=int(sys.stdin.readline())
+    if a==0:
+        if len(array)==0:
+            print(0)
+        else:
+            print(-1*heapq.heappop(array))
+    else:
+        heapq.heappush(array, -a)
