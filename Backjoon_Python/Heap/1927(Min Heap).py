d029d73544294456f3b0c7a58a23f9d3e3d61d80
khsung <khsung0@nate.com>
2021-02-06 13:32:47 +0900

    최소 힙(합격)

---
+++ Backjoon_Python/Heap/1927(Min Heap).py
@@ -0,0 +1,15 @@
+#1927 최소 힙
+import heapq
+import sys
+num=int(input())
+array=[]
+res=[]
+for i in range(num):
+    a=int(sys.stdin.readline())
+    if a==0:
+        if len(array)==0:
+            print(0)
+        else:
+            print(heapq.heappop(array))
+    else:
+        heapq.heappush(array, a) 
