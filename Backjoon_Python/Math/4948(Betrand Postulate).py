ff55061fb455967014eb63327d986f752fb863e0
khsung <khsung0@nate.com>
2021-02-08 09:38:42 +0900

    베르트랑 공준(합격)

--- Backjoon_Python/Math/4948(Betrand Postulate).py
+++ Backjoon_Python/Math/4948(Betrand Postulate).py
@@ -1,56 +1,21 @@
-#4948 Betrand Postulate
-num=1
-res=[]
-rescount=[]
+#4948 베르트랑 공준
+prime=[1 for i in range(246913)]
+prime[0]=0
+prime[1]=0
+for i in range(2,len(prime)):
+    if prime[i]==1:
+        temp=i
+        tempindex=2*temp
+        while tempindex<246913:
+            prime[tempindex]=0
+            tempindex+=temp
 while True:
-    num=int(input())
-    if num==0:
+    cnt=0
+    a=int(input())
+    if a==0:
         break
     else:
-        count=0
-        a=num
-        b=2*num
-        prime=[1 for i in range(b+1)]
-        prime[0]=0
-        prime[1]=0
-        for i in range(2,b+1):
-            temp=2*i
-            while temp<=b:
-                prime[temp]=0
-                temp+=i
-        for i in range(a,b+1):
+        for i in range(a+1,2*a+1):
             if prime[i]==1:
-                res.append(i)
-
-
-for i in range(len(res)):
-    print(rescount[i])
-
-
-
-#num=1
-#res=[]
-#while True:
-#    num=int(input())
-#    if num==0:
-#        break
-#    else:
-#        count=0
-#        a=num
-#        b=2*num
-#        prime=[1 for i in range(b+1)]
-#        prime[0]=0
-#        prime[1]=0
-#        for i in range(2,b+1):
-#            check=0
-#            temp=2*i
-#            while temp<=b:
-#                prime[temp]=0
-#                temp+=i
-#        for i in range(a+1,b+1):
-#            if prime[i]==1:
-#                count+=1
-#        res.append(count)
-
-#for i in range(len(res)):
-#    print(res[i])
+                cnt+=1
+        print(cnt)
