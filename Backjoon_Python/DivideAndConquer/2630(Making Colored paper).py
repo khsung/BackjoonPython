f9236ae049d1867eaacc263059d819a2d1210fbd
khsung <khsung0@nate.com>
2021-02-12 18:14:53 +0900

    색종이 만들기(합격)

--- Backjoon_Python/DivideAndConquer/2630(Making Colored paper).py
+++ Backjoon_Python/DivideAndConquer/2630(Making Colored paper).py
@@ -1,52 +1,33 @@
 #2630 색종이 만들기
-
-
-color = [0,0]
 square = []
-def findsquare(rleft, rright,cleft, cright):
-    currcolor=square[rleft][cleft]
-    check=0
-    for i in range(rleft, rright + 1):
-        for j in range(cleft, cright+1):
-            if square[i][j]!=currcolor:
-                mid = (rleft + rright) // 2
-                findsquare(rleft, mid,cleft,mid)
-                findsquare(rleft, mid,mid + 1, cright)
-                findsquare(mid + 1, rright,cleft,mid)
-                findsquare(mid + 1, rright,mid + 1, cright)
-                check=1
-                return
-    if check==0:
-        color[currcolor]+=1
+color = [0,0]
 num = int(input())
 for i in range(num):
-    temp = input().split()
-    square.append(list(map(int,temp)))
-findsquare(0, num - 1,0, num - 1)
+    temp = list(map(int,input().split()))
+    square.append(temp)
+
+def squarefunc(rfirst,rsecond,cfirst,csecond):
+    global square
+    global color
+    whitesum=0
+    bluesum=0
+    for i in range(rfirst,rsecond+1):
+        temp=square[i][cfirst:csecond+1].count(0)
+        whitesum+=temp
+        bluesum=bluesum+(csecond+1-cfirst)-temp
+    if bluesum==0:
+        color[0]+=1
+        return
+    elif whitesum==0:
+        color[1]+=1
+        return
+    else:
+        rmid=(rfirst+rsecond)//2
+        cmid=(cfirst+csecond)//2
+        squarefunc(rfirst,rmid,cfirst,cmid)
+        squarefunc(rfirst,rmid,cmid+1,csecond)
+        squarefunc(rmid+1,rsecond,cfirst,cmid)
+        squarefunc(rmid+1,rsecond,cmid+1,csecond)
+squarefunc(0,num - 1,0,num - 1)
 print(color[0])
 print(color[1])
-
-
-
-#white = 0
-#blue = 0
-#square = []
-#def findsquare(left, right):
-#    temp = []
-#    for i in range(left, right + 1):
-#        temp.append(square[i][left:right + 1])
-#    if 1 not in temp:
-#        white+=1
-#    elif 0 not in temp:
-#        blue+=1
-#    else:
-#        mid = (left + right) // 2
-#        findsquare(left, mid)
-#        findsquare(mid + 1, right)
-#num = int(input())
-#for i in range(num):
-#    temp = input().split()
-#    square.append(list(map(int,temp)))
-#findsquare(0, num - 1)
-#print(white)
-#print(blue)
