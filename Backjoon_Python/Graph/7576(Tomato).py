3a92a9c2812197d13be320495172c9f080f0f714
khsung <khsung0@nate.com>
2021-02-14 21:24:47 +0900

    토마토(시간초과)

--- Backjoon_Python/Graph/7576(Tomato).py
+++ Backjoon_Python/Graph/7576(Tomato).py
@@ -10,33 +10,29 @@ for i in range(n):
     temp1=copy.deepcopy(temp)
     square.append(temp1)
     for j in range(temp.count(1)):
-        tomatoinfo.append([i,temp.index(1)])
+        tomatoinfo.append([i,temp.index(1),0])
         temp[temp.index(1)]=0
-cnt=0
 while len(tomatoinfo)>0:
-    size=len(tomatoinfo)
-    for i in range(size):
-        x=tomatoinfo[0][0]
-        y=tomatoinfo[0][1]
-        tomatoinfo.pop(0)
-        if x-1>=0 and square[x-1][y]==0 and visited[x-1][y]==0:
-            visited[x-1][y]=1
-            square[x-1][y]=1
-            tomatoinfo.append([x-1,y])
-        if y+1<m and square[x][y+1]==0 and visited[x][y+1]==0:
-            visited[x][y+1]=1
-            square[x][y+1]=1
-            tomatoinfo.append([x,y+1])
-        if x+1<n and square[x+1][y]==0 and visited[x+1][y]==0:
-            visited[x+1][y]=1
-            square[x+1][y]=1
-            tomatoinfo.append([x+1,y])
-        if y-1>=0 and square[x][y-1]==0 and visited[x][y-1]==0:
-            visited[x][y-1]=1
-            square[x][y-1]=1
-            tomatoinfo.append([x,y-1])
-    if len(tomatoinfo)>0:
-        cnt+=1
+    x=tomatoinfo[0][0]
+    y=tomatoinfo[0][1]
+    cnt=tomatoinfo[0][2]
+    tomatoinfo.pop(0)
+    if x-1>=0 and square[x-1][y]==0 and visited[x-1][y]==0:
+        visited[x-1][y]=1
+        square[x-1][y]=1
+        tomatoinfo.append([x-1,y,cnt+1])
+    if y+1<m and square[x][y+1]==0 and visited[x][y+1]==0:
+        visited[x][y+1]=1
+        square[x][y+1]=1
+        tomatoinfo.append([x,y+1,cnt+1])
+    if x+1<n and square[x+1][y]==0 and visited[x+1][y]==0:
+        visited[x+1][y]=1
+        square[x+1][y]=1
+        tomatoinfo.append([x+1,y,cnt+1])
+    if y-1>=0 and square[x][y-1]==0 and visited[x][y-1]==0:
+        visited[x][y-1]=1
+        square[x][y-1]=1
+        tomatoinfo.append([x,y-1,cnt+1])
 for i in range(n):
     if square[i].count(0)>0:      
         check=False
