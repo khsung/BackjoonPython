c10469884b2dd08ccd5fb147ac441deeaa16c44c
khsung <khsung0@nate.com>
2021-02-15 10:18:33 +0900

    프린터 큐(합격)

--- Backjoon_Python/StackAndQueue/1966(Printer Queue).py
+++ Backjoon_Python/StackAndQueue/1966(Printer Queue).py
@@ -5,3 +5,25 @@ for i in range(testcase):
     n,m=map(int,input().split())
     printerqueue=list(map(int, input().split()))
     importance=copy.deepcopy(printerqueue)
+    importance.sort(reverse=True)
+    cnt=1
+    res=cnt
+    while len(printerqueue)>0:
+        if importance[0]==printerqueue[0]:
+            if m==0:
+                res=cnt
+                break
+            else:
+                importance.pop(0)
+                printerqueue.pop(0)
+                n-=1
+                m-=1
+                cnt+=1
+                if m<0:
+                    m+=n
+        else:
+            printerqueue.append(printerqueue.pop(0))
+            m-=1
+            if m<0:
+                m+=n
+    print(res)
