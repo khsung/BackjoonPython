e38d4383280f607b2365c8002f47f5d09984dd87
khsung <khsung0@nate.com>
2021-02-17 05:33:37 +0900

    플로이드(미완성)

--- Backjoon_Python/Graph/11404(Floyd).py
+++ Backjoon_Python/Graph/11404(Floyd).py
@@ -1,6 +1,6 @@
-#11404 플로이드
+#11404 플로이드 미완성
 n=int(input())
-graph=[[0 for i in range(n)]for j in range(n)]
+graph=[[100000 for i in range(n)]for j in range(n)]
 m=int(input())
 for i in range(m):
     a,b,c=map(int,input().split())
@@ -10,12 +10,12 @@ for i in range(m):
         if graph[a-1][b-1]>c:
             graph[a-1][b-1]=c
 
-#for i in range(len(graph)):
-#    for j in range(len(graph)):
-#        print(graph[i][j],end=" ")
-#    print()
+for i in range(len(graph)):
+    for j in range(len(graph)):
+        print(graph[i][j],end=" ")
+    print()
 
-#print("==============")
+print("==============")
 
 #이부분 문제
 for i in range(len(graph)):
@@ -24,6 +24,7 @@ for i in range(len(graph)):
             if graph[j][i]!=0 and graph[i][k]!=0 and graph[j][k]!=0 and graph[j][i]+graph[i][k]<graph[j][k]:
                 print(i,j,k,graph[j][i]+graph[i][k],graph[j][k])
                 graph[j][k]=graph[j][i]+graph[i][k]
+            
 
 
 print("==============")
