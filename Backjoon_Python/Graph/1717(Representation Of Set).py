e17dfcc8877e687bbb76b9b4f76f929e76f98084
khsung <khsung0@nate.com>
2021-02-18 22:28:15 +0900

    집합의 표현(시간초과)

--- Backjoon_Python/Graph/1717(Representation Of Set).py
+++ Backjoon_Python/Graph/1717(Representation Of Set).py
@@ -1,10 +1,26 @@
-#1717 집합의 표현 미완성
-n,m=int(input().split())
+#1717 집합의 표현
+n,m=map(int,input().split())
 union_find=[i for i in range(n+1)]
 for i in range(m):
-    op,a,b=int(input().split())
+    op,a,b=map(int,input().split())
     if a>b:
         a,b=b,a
     if op==0:
-
+        parenta=union_find[a]
+        parentb=union_find[b]
+        if parenta<parentb:
+            while union_find.count(parentb)!=0:
+                union_find[union_find.index(parentb)]=parenta
+        elif parenta>parentb:
+            parent=parentb
+            while union_find.count(parentb)!=0:
+                union_find[union_find.index(parenta)]=parentb
     else:
+        while a!=union_find[a]:
+            a=union_find[a]
+        while b!=union_find[b]:
+            b=union_find[b]
+        if a==b:
+            print("YES")
+        else:
+            print("NO")
