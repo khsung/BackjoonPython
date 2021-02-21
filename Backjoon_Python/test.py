b43969b68736b3bb08433e16349aba3971c86372
khsung <khsung0@nate.com>
2021-02-21 16:13:55 +0900

    트리의 부모 찾기(실패)

---
+++ Backjoon_Python/test.py
@@ -0,0 +1,23 @@
+
+
+#=======================================================
+#n,k=map(int, input().split())   C 합격
+#array=list(map(int, input().split()))
+#res=0
+#temp=0
+#check=[0 for i in range(100001)]
+#start=0
+#end=0
+#while start<n:
+#    if check[array[start]]+1<=k:
+#        check[array[start]]+=1
+#        start+=1
+#        temp+=1
+#        if temp>res:
+#            res=temp
+#    else:
+#        check[array[end]]-=1
+#        end+=1
+#        temp-=1
+#print(res)
+#==============================================================
