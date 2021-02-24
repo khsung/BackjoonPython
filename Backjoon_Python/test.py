de2460910acfeeebcd93789a5729ba6f61a9b01c
khsung <khsung0@nate.com>
2021-02-24 23:52:44 +0900

    테스트용

--- Backjoon_Python/test.py
+++ Backjoon_Python/test.py
@@ -8,3 +8,15 @@
 #print(tree.data)
 #print(tree.lchild)
 #print(tree.rchild.data)
+n=int(input())
+res=[0,0]
+for i in range(n):
+    a=int(input())
+    if a==1:
+        res[1]+=1
+    else:
+        res[0]+=1
+if res[0]>res[1]:
+    print("J")
+else:
+    print("")
