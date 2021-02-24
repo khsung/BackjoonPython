60a4ebb97f09050a3edb85d859f4c66e1d25f01c
khsung <khsung0@nate.com>
2021-02-24 17:00:04 +0900

    테스트용

--- Backjoon_Python/test.py
+++ Backjoon_Python/test.py
@@ -1,10 +1,10 @@
-class Tree:
-    def __init__(self,data,lchild,rchild):
-        self.data=data
-        self.lchild=lchild
-        self.rchild=rchild
-tree=Tree(1,2,Tree(3,4,5))
-
-print(tree.data)
-print(tree.lchild)
-print(tree.rchild.data)
+#class Tree:
+#    def __init__(self,data,lchild,rchild):
+#        self.data=data
+#        self.lchild=lchild
+#        self.rchild=rchild
+#tree=Tree(1,2,Tree(3,4,5))
+#
+#print(tree.data)
+#print(tree.lchild)
+#print(tree.rchild.data)
