b4b29137743e3034ba0b8aaf7843900d1d9fc9c0
khsung <khsung0@nate.com>
2021-02-24 12:19:43 +0900

    큐(합격)

--- Backjoon_Python/StackAndQueue/10845(Queue).py
+++ Backjoon_Python/StackAndQueue/10845(Queue).py
@@ -11,3 +11,20 @@ for i in range(n):
             print(-1)
         else:
             print(queue.pop(0))
+    elif op[0]=="size":
+        print(len(queue))
+    elif op[0]=="empty":
+        if len(queue)==0:
+            print(1)
+        else:
+            print(0)
+    elif op[0]=="front":
+        if len(queue)==0:
+            print(-1)
+        else:
+            print(queue[0])
+    else:
+        if len(queue)==0:
+            print(-1)
+        else:
+            print(queue[len(queue)-1])
