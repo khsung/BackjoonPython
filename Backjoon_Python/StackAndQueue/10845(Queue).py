a9c3e27bca12c01f82014b2a7b65032c645c03cc
khsung <khsung0@nate.com>
2021-02-24 12:19:24 +0900

    트리 순회(미완성)

--- Backjoon_Python/StackAndQueue/10845(Queue).py
+++ Backjoon_Python/StackAndQueue/10845(Queue).py
@@ -1,6 +1,13 @@
 #10845 큐
+import sys
 n=int(input())
 queue=[]
 for i in range(n):
-    op=list(input().split())
-    print(op)
+    op=list(sys.stdin.readline().split())
+    if op[0]=="push":
+        queue.append(op[1])
+    elif op[0]=="pop":
+        if len(queue)==0:
+            print(-1)
+        else:
+            print(queue.pop(0))
--- Backjoon_Python/Tree/1991(Tree Traversal).py
+++ Backjoon_Python/Tree/1991(Tree Traversal).py
@@ -1,7 +1,6 @@
 #1991 트리 순회 미완성, 다른 트리 구현 방법 생각
 #class Tree():
 #    def __init__(self, parent,lchild=None,rchild=None):
-
 n=int(input())
 for i in range(n):
     parent,lchild,rchild=input().split()
