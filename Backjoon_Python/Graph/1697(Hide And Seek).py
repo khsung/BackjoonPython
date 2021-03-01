aa30f9823a9cba2b3a213411683003c52c7bf0f2
khsung <khsung0@nate.com>
2021-03-02 01:45:29 +0900

    숨바꼭질(틀림)

--- Backjoon_Python/Graph/1697(Hide And Seek).py
+++ Backjoon_Python/Graph/1697(Hide And Seek).py
@@ -3,5 +3,24 @@ n,k=map(int, input().split())
 queue=[[n,0]]
 cnt=0
 res=0
+visited=[0 for i in range(k*2)]
 while True:
+    print(queue)
     temp=queue.pop(0)
+    if temp[0]==k:
+        res=temp[1]
+        break
+    else:
+        if 2*temp[0]<=k and visited[2*temp[0]]==0:
+            queue.append([2*temp[0],temp[1]+1])
+            visited[2*temp[0]]=1
+        if temp[0]+1<=k and visited[temp[0]+1]==0:
+            queue.append([temp[0]+1,temp[1]+1])
+            visited[temp[0]+1]=1
+        if temp[0]-1<=k and temp[0]-1>0 and visited[temp[0]-1]==0:
+            queue.append([temp[0]-1,temp[1]+1])
+            visited[temp[0]-1]=1
+        print(temp[0]-1,k,visited[temp[0]-1])
+    print("end")
+print(res)
+#5 3 일떄 안됨
