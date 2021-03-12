402396f79b4cce6dac3cea5e30a90b12b9b53eff
khsung <khsung0@nate.com>
2021-03-12 10:23:07 +0900

    과제(합격)

--- Backjoon_Python/GreedyAlgorithm/13904(Assignment).py
+++ Backjoon_Python/GreedyAlgorithm/13904(Assignment).py
@@ -1,18 +1,26 @@
-#13904 과제(미완성)
+#13904 과제
 n=int(input())
 assign=[]
 maxday=0
 res=0
-cnt=0
+
 for i in range(n):
     temp=list(map(int,input().split()))
     if maxday<temp[0]:
         maxday=temp[0]
     assign.append(temp)
-assign=sorted(assign,key=lambda x:(-x[1],x[0]))
-for i in range(len(assign)):
-    if assign[i][0]>cnt:
-        cnt+=1
-        res+=assign[i][1]
-print(assign)
+assign=sorted(assign,key=lambda x:(-x[0],-x[1]))
+day=assign[0][0]
+index=0
+for i in range(day,0,-1):
+    max=0
+    temparr=[]
+    for j in assign:
+        if j[0]>=i:
+            if j[1]>max:
+                max=j[1]
+                temparr=[j[0],j[1]]
+    if len(temparr)>0:
+        res+=temparr[1]
+        assign.remove(temparr)
 print(res)
