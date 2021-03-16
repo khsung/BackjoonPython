3f29be9081d105d55eaf2c479792fd674d14c254
khsung <khsung0@nate.com>
2021-03-16 11:28:57 +0900

    트리 순회(미완성)

--- Backjoon_Python/Tree/1991(Tree Traversal).py
+++ Backjoon_Python/Tree/1991(Tree Traversal).py
@@ -1,12 +1,28 @@
-#1991 트리 순회 미완성, 다른 트리 구현 방법 생각
-#class Tree():
-#    def __init__(self, parent,lchild=None,rchild=None):
-n=int(input())
-for i in range(n):
-    parent,lchild,rchild=input().split()
-    if lchild=='.':
-        lchild=None
-    if rchild=='.':
-        rchild=None
+#1991 트리 순회 미완성
 
+def preorder(tree,root):
+    print(root,end="")
+    if tree[root][0]!=".":
+        preorder(tree,tree[root][0])
+    if tree[root][1]!=".":
+        preorder(tree,tree[root][1])
     
+
+def inorder(tree):
+    return 0
+
+def postorder(tree):
+    return 0
+
+n=int(input())
+tree={}
+for i in range(n):
+    root,left,right=input().split()
+    tree[root]=[left,right]
+    if i==0:
+        saveroot=root
+preorder(tree,saveroot)
+print()
+
+#temp={'A':[1,2]}
+#print(temp['A'][0])
