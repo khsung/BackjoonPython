bdc90c7d31e3b064d2319f4d84d7d4fcd6352f8a
khsung <khsung0@nate.com>
2021-03-22 17:06:25 +0900

    퇴사(실패)

--- Backjoon_Python/BruteForce/14501(Leave).py
+++ Backjoon_Python/BruteForce/14501(Leave).py
@@ -1,4 +1,4 @@
-#14501 퇴사 미완성
+#14501 퇴사
 schedule=[]
 n=int(input())
 maxsum=0
@@ -7,13 +7,12 @@ for i in range(n):
     schedule.append([a,b])
 for i in range(n):
     tempsum=0
-    day=i
+    day=i-1
+    currdate=i
     for j in range(i,n):
-        if day+schedule[j][0]-1<n-1 and day<=j:
+        if day+schedule[j][0]<n and day<j:
             day=day+schedule[j][0]
             tempsum+=schedule[j][1]
-            print(j,day,schedule[j][0],schedule[j][1])
     if maxsum<tempsum:
         maxsum=tempsum
-    print("tempsum =",tempsum)
 print(maxsum)
