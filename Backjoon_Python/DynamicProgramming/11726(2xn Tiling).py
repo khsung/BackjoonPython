77c485c9145569362f4e078b2635e8ad62d4243d
khsung <khsung0@nate.com>
2021-03-24 10:05:30 +0900

    2xn 타일링(합격)

--- Backjoon_Python/DynamicProgramming/11726(2xn Tiling).py
+++ Backjoon_Python/DynamicProgramming/11726(2xn Tiling).py
@@ -1,2 +1,13 @@
 #11726 2Xn 타일링
 n=int(input())
+dp=[0 for i in range(n+2)]
+index=0
+while index<=n:
+    index+=1
+    if index==1:
+        dp[index]=1
+    elif index==2:
+        dp[index]=2
+    else:
+        dp[index]=(dp[index-1]+dp[index-2])%10007
+print(dp[n])
--- Backjoon_Python/test.py
+++ Backjoon_Python/test.py
@@ -8,15 +8,31 @@
 #print(tree.data)
 #print(tree.lchild)
 #print(tree.rchild.data)
-from urllib import request
-from bs4 import BeautifulSoup
 
-target=request.urlopen("http://www.weather.go.kr/weather/lifenindustry/sevice_rss.jsp")
-soup=BeautifulSoup(target,"html.parser")
-print(soup)
-for location in soup.select("location"):
-    print("도시:", location.select_one("city").string)
-    print("날씨:", location.select_one("wf").string)
-    print("최저기온:", location.select_one("tmn").string)
-    print("최고기온:", location.select_one("tmx").string)
-    print()
+
+
+#from urllib import request
+#from bs4 import BeautifulSoup
+
+#target=request.urlopen("http://www.weather.go.kr/weather/lifenindustry/sevice_rss.jsp")
+#soup=BeautifulSoup(target,"html.parser")
+#print(soup)
+#for location in soup.select("location"):
+#    print("도시:", location.select_one("city").string)
+#    print("날씨:", location.select_one("wf").string)
+#    print("최저기온:", location.select_one("tmn").string)
+#    print("최고기온:", location.select_one("tmx").string)
+#    print()
+
+from tkinter import *
+window=Tk()
+window.title("test")
+window.geometry("400x400")
+window.resizable(width=False, height=False)
+label1=Label(window,text="Python",font=("궁서체",18),bg="red",width=10,height=10)
+label2=Label(window,text="java",font=("HY나무B",20),bg="yellow")
+label3=Label(window,text="java",font=("HY나무B",20),bg="yellow",anchor=LEFT)
+label1.pack()
+label2.pack()
+label3.pack()
+window.mainloop()
