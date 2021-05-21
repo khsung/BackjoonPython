28b802983e6d1a7013c12919ca25bb78cde085e9
khsung <khsung0@nate.com>
2021-05-21 22:32:59 +0900

    유기농배추(불합격)

--- Backjoon_Python/Graph/1012(Organic Cabbage).py
+++ Backjoon_Python/Graph/1012(Organic Cabbage).py
@@ -1,6 +1,7 @@
-#1012 유기농배추 구현중
+#1012 유기농배추 불합격
 testcase=int(input())
 queue=[]
+cnt_queue=[]
 for i in range(testcase):
     m,n,k=input().split()
     m=int(m)
@@ -11,26 +12,25 @@ for i in range(testcase):
     for j in range(k):
         target=list(map(int,input().split()))
         graph[target[1]][target[0]]=1
-        for y in range(n):
-            for x in range(m):
-                if graph[y][x]==1:
-                    queue.append([y,x])
-                    while len(queue)>0:
-                        tempy,tempx=queue.pop(0)
-                        graph[tempy][tempx]=0
-                        if tempx-1>=0 and graph[tempy][tempx-1]==1:
-                            queue.append([tempy,tempx-1])
+    for y in range(n):
+        for x in range(m):
+            if graph[y][x]==1:
+                cnt+=1
+                queue.append([y,x])
+                while len(queue)>0:
+                    tempy,tempx=queue.pop(0)
+                    graph[tempy][tempx]=0
+                    if tempx-1>=0 and graph[tempy][tempx-1]==1:
+                        queue.append([tempy,tempx-1])
+                    elif tempx+1<m and graph[tempy][tempx+1]==1:
+                        queue.append([tempy,tempx+1])
+                    elif tempy-1>=0 and graph[tempy-1][tempx]==1:
+                        queue.append([tempy-1,tempx])
+                    elif tempy+1<n and graph[tempy+1][tempx]==1:
+                        queue.append([tempy+1,tempx])
+    #print(graph)
     print(cnt)
-    #for j in range(k):
-    #    target=list(map(int,input().split()))
-    #    graph[target[1]][target[0]]=1
-    #    for y in range(n):
-    #        for x in range(m):
-    #            if graph[y][x]==1:
-    #                queue.append([y,x])
-    #                while len(queue)>0:
-    #                    tempy,tempx=queue.pop(0)
-    #                    graph[tempy][tempx]=0
-    #                    if tempx-1>=0 and graph[tempy][tempx-1]==1:
-    #                        queue.append([tempy,tempx-1])
-    #print(cnt)
+#    cnt_queue.append(cnt)
+#print("res")
+#for i in cnt_queue:
+#    print(i)
