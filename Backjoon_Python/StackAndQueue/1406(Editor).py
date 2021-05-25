1a98b351ce19b7ef977bca1c09c8a2b7ed063448
khsung <khsung0@nate.com>
2021-05-25 22:01:21 +0900

    에디터(합격)

--- Backjoon_Python/StackAndQueue/1406(Editor).py
+++ Backjoon_Python/StackAndQueue/1406(Editor).py
@@ -1,16 +1,23 @@
-#1406 에디터 스택 사용하기
-left_stack=list(input())
+#1406 에디터
+import sys
+left_stack=list(sys.stdin.readline().strip())
 right_stack=[]
-print(left_stack)
 num=int(input())
 for i in range(num):
-    command=input()
+    command=sys.stdin.readline().strip()
     if len(command)==3:
         cmd=command.split()
-
+        left_stack.append(cmd[1])
     else:
         if command=="L":
-
+            if len(left_stack)!=0:
+                right_stack.append(left_stack.pop())
         elif command=="D":
-
+            if len(right_stack)!=0:
+                left_stack.append(right_stack.pop())
         else:
+            if len(left_stack)!=0:
+                left_stack.pop()
+left_stack=left_stack+list(reversed(right_stack))
+for i in left_stack:
+    print(i,end="")
