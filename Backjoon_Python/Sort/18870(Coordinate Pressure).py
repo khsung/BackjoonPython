cd40ace6726d43ef3530962a04e27212eaa4f16c
khsung <khsung0@nate.com>
2021-05-26 22:37:39 +0900

    좌표 압축(시간 초과)

--- Backjoon_Python/Sort/18870(Coordinate Pressure).py
+++ Backjoon_Python/Sort/18870(Coordinate Pressure).py
@@ -1,6 +1,8 @@
 #18870 좌표 압축
 num=int(input())
-a=[1,2,3,3]
-print(a)
-a=set(a)
-print(a)
+arr=list(map(int,input().split()))
+set_arr=set(arr)
+set_arr=list(set_arr)
+set_arr.sort()
+for i in range(num):
+    print(set_arr.index(arr[i]),end=" ")
