5f5af0502db743a1928f17cd19300f66d74c0239
khsung <khsung0@nate.com>
2021-05-27 22:57:35 +0900

    좌표 압푹(합격)

--- Backjoon_Python/Sort/18870(Coordinate Pressure).py
+++ Backjoon_Python/Sort/18870(Coordinate Pressure).py
@@ -1,8 +1,14 @@
 #18870 좌표 압축
+import sys
 num=int(input())
-arr=list(map(int,input().split()))
-set_arr=set(arr)
-set_arr=list(set_arr)
+arr=list(map(int,sys.stdin.readline().split()))
+set_arr=list(set(arr))
 set_arr.sort()
+dic_arr={}
+count=0
+#딕셔너리를 이용한 시간 단축
+for i in set_arr:
+    dic_arr[i]=count
+    count+=1
 for i in range(num):
-    print(set_arr.index(arr[i]),end=" ")
+    print(dic_arr[arr[i]],end=" ")
