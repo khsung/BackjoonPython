960381efb0dafa342318603419c680889e3470ea
khsung <khsung0@nate.com>
2021-05-29 22:55:44 +0900

    숫자 카드(합격)

--- Backjoon_Python/BinarySearch/10815(Number Card).py
+++ Backjoon_Python/BinarySearch/10815(Number Card).py
@@ -4,6 +4,19 @@ n_arr=list(map(int,input().split()))
 m=int(input())
 m_arr=list(map(int,input().split()))
 n_arr.sort()
-m_arr.sort()
-print(n_arr)
-print(m_arr)
+for i in range(len(m_arr)):
+    left=0
+    right=len(n_arr)-1
+    check=False
+    while left<=right:
+        mid=(left+right)//2
+        if n_arr[mid]==m_arr[i]:
+            print(1,end=" ")
+            check=True
+            break
+        elif n_arr[mid]<m_arr[i]:
+            left=mid+1
+        else:
+            right=mid-1
+    if not check:
+        print(0,end=" ")
