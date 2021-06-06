a9d05fe3f0b3ededfa77b46db07aece04e7ccd77
khsung <khsung0@nate.com>
2021-06-06 16:09:13 +0900

    숫자 카드2 (수정 중)

--- Backjoon_Python/BinarySearch/10816(Number Card2).py
+++ Backjoon_Python/BinarySearch/10816(Number Card2).py
@@ -1,14 +1,12 @@
 #10816 숫자 카드2 수정중
+import sys
 n=int(input())
-n_arr=list(map(int,input().split()))
+n_arr=list(map(int,sys.stdin.readline().split()))
 m=int(input())
-m_arr=list(map(int,input().split()))
-n_arr_set=list(set(n_arr))
+m_arr=list(map(int,sys.stdin.readline().split()))
 n_arr.sort()
 n_arr_dic={}
 n_arr_index=0
-n_arr_set_index=0
-print(n_arr)
 while n_arr_index<len(n_arr):
     cnt=0
     for i in range(n_arr_index,len(n_arr)):
@@ -21,8 +19,11 @@ while n_arr_index<len(n_arr):
         elif i==len(n_arr)-1:
             n_arr_index=i+1
             n_arr_dic[n_arr[i-1]]=cnt+1
+            break
         else:
             cnt+=1
-    print("n_arr_index=",n_arr_index)
-    print("cnt=",cnt)
-print(n_arr_dic)
+for i in range(len(m_arr)):
+    if m_arr[i] in n_arr_dic:
+        print(n_arr_dic[m_arr[i]],end=" ")
+    else:
+        print(0,end=" ")
