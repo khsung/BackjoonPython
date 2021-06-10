dd4413939ad8f2a8ad0d7aa520559c6b76eddf30
khsung <khsung0@nate.com>
2021-06-11 02:03:28 +0900

    유기농배추(합격)

--- Backjoon_Python/Graph/1012(Organic Cabbage).py
+++ Backjoon_Python/Graph/1012(Organic Cabbage).py
@@ -1,61 +1,31 @@
-#1012 유기농배추 불합격
+#1012 유기농배추
 testcase=int(input())
-queue=[]
 for i in range(testcase):
-    m,n,k=input().split()
-    m=int(m)
-    n=int(n)
-    k=int(k)
-    cnt=0
-    graph=[[0 for a in range(m)]for b in range(n)]
-    for j in range(k):
-        target=list(map(int,input().split()))
-        graph[target[1]][target[0]]=1
-    for y in range(n):
-        for x in range(m):
-            if graph[y][x]==1:
-                cnt+=1
-                queue.append([y,x])
+    queue=[]
+    res=0
+    M,N,K=map(int,input().split())
+    graph=[[0 for j in range(M)]for k in range(N)]
+    for j in range(K):
+        tempm,tempn=map(int,input().split())
+        graph[tempn][tempm]=1
+    for j in range(N):
+        for k in range(M):
+            if graph[j][k]==1:
+                res+=1
+                queue.append([j,k])
+                graph[j][k]=0
                 while len(queue)>0:
-                    tempy,tempx=queue.pop(0)
-                    graph[tempy][tempx]=0
-                    if tempx-1>=0 and graph[tempy][tempx-1]==1:
-                        queue.append([tempy,tempx-1])
-                    elif tempx+1<m and graph[tempy][tempx+1]==1:
-                        queue.append([tempy,tempx+1])
-                    elif tempy-1>=0 and graph[tempy-1][tempx]==1:
-                        queue.append([tempy-1,tempx])
-                    elif tempy+1<n and graph[tempy+1][tempx]==1:
-                        queue.append([tempy+1,tempx])
-    print(cnt)
-
-
-#testcase=int(input())
-#queue=[]
-#for i in range(testcase):
-#    m,n,k=input().split()
-#    m=int(m)
-#    n=int(n)
-#    k=int(k)
-#    cnt=0
-#    graph=[[0 for a in range(m)]for b in range(n)]
-#    for j in range(k):
-#        target=list(map(int,input().split()))
-#        graph[target[1]][target[0]]=1
-#    for y in range(n):
-#        for x in range(m):
-#            if graph[y][x]==1:
-#                cnt+=1
-#                queue.append([y,x])
-#                while len(queue)>0:
-#                    tempy,tempx=queue.pop(0)
-#                    graph[tempy][tempx]=0
-#                    if tempx-1>=0 and graph[tempy][tempx-1]==1:
-#                        queue.append([tempy,tempx-1])
-#                    elif tempx+1<m and graph[tempy][tempx+1]==1:
-#                        queue.append([tempy,tempx+1])
-#                    elif tempy-1>=0 and graph[tempy-1][tempx]==1:
-#                        queue.append([tempy-1,tempx])
-#                    elif tempy+1<n and graph[tempy+1][tempx]==1:
-#                        queue.append([tempy+1,tempx])
-#    print(cnt)
+                    tempn,tempm=queue.pop()
+                    if tempn-1>=0 and graph[tempn-1][tempm]==1:
+                        graph[tempn-1][tempm]=0
+                        queue.append([tempn-1,tempm])
+                    if tempn+1<N and graph[tempn+1][tempm]==1:
+                        graph[tempn+1][tempm]=0
+                        queue.append([tempn+1,tempm])
+                    if tempm-1>=0 and graph[tempn][tempm-1]==1:
+                        graph[tempn][tempm-1]=0
+                        queue.append([tempn,tempm-1])
+                    if tempm+1<M and graph[tempn][tempm+1]==1:
+                        graph[tempn][tempm+1]=0
+                        queue.append([tempn,tempm+1])
+    print(res)
