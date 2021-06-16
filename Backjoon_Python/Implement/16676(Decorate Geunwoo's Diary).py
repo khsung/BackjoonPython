aabc43cfbdb76a357dea03f8301eacdc4bb2a996
khsung <khsung0@nate.com>
2021-06-16 17:56:20 +0900

    근우의 다이어리 꾸미기(합격)

--- Backjoon_Python/Implement/16676(Decorate Geunwoo's Diary).py
+++ Backjoon_Python/Implement/16676(Decorate Geunwoo's Diary).py
@@ -1,7 +1,16 @@
 #16676 근우의 다이어리 꾸미기
 n=int(input())
-num_array=[0 for i in range(10)]
-while n>0:
-    num_array[int(n%10)]+=1
-    n=n//10
-print(max(num_array))
+temp_n=n
+stand=0
+digit=0
+while temp_n>0:
+    temp_n=temp_n//10
+    digit+=1
+for i in range(digit):
+    stand=stand*10+1
+if n<10:
+    print(1)
+elif stand>n:
+    print(digit-1)
+else:
+    print(digit)
