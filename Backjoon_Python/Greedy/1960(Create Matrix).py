c09f0a045640985d9c2cc2d13a076d3e4b0d0c60
khsung <khsung0@nate.com>
2021-06-18 16:30:50 +0900

    행렬 만들기(불합격)

--- Backjoon_Python/Greedy/1960(Create Matrix).py
+++ Backjoon_Python/Greedy/1960(Create Matrix).py
@@ -7,10 +7,14 @@ for i in range(n):
     for j in range(n):
         if col[j]>0 and row[i]>0:
             matrix[i][j]=1
-            col[i]-=1
+            col[j]-=1
             row[i]-=1
 
-for i in range(n):
-    for j in range(n):
-        print(matrix[i][j],end=" ")
-    print()
+if row.count(0)==n and col.count(0)==n:
+    print(1)
+    for i in range(n):
+        for j in range(n):
+            print(matrix[i][j],end=" ")
+        print()
+else:
+    print(-1)
