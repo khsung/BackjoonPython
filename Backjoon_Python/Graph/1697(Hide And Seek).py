01e6fd7d0c64c069c9599f2e7b69779be41e7b06
khsung <khsung0@nate.com>
2021-06-23 01:31:12 +0900

    숨바꼭질(합격)

--- Backjoon_Python/Graph/1697(Hide And Seek).py
+++ Backjoon_Python/Graph/1697(Hide And Seek).py
@@ -1,26 +1,23 @@
 #1697 숨바꼭질
 n,k=map(int, input().split())
-queue=[[n,0]]
-cnt=0
-res=0
-visited=[0 for i in range(k*2)]
-while True:
-    print(queue)
-    temp=queue.pop(0)
-    if temp[0]==k:
-        res=temp[1]
-        break
-    else:
-        if 2*temp[0]<=k and visited[2*temp[0]]==0:
-            queue.append([2*temp[0],temp[1]+1])
-            visited[2*temp[0]]=1
-        if temp[0]+1<=k and visited[temp[0]+1]==0:
-            queue.append([temp[0]+1,temp[1]+1])
-            visited[temp[0]+1]=1
-        if temp[0]-1<=k and temp[0]-1>0 and visited[temp[0]-1]==0:
-            queue.append([temp[0]-1,temp[1]+1])
-            visited[temp[0]-1]=1
-        print(temp[0]-1,k,visited[temp[0]-1])
-    print("end")
-print(res)
-#5 3 일떄 안됨
+visited=[0 for i in range(100001)]
+visited[n]=1
+if n==k:
+    print(0)
+else:
+    queue=[[n,0]]
+    while len(queue)>0:
+        temp_n,time=queue.pop(0)
+        if temp_n==k:
+            print(time)
+            break
+        else:
+            if temp_n-1>=0 and visited[temp_n-1]==0:
+                visited[temp_n-1]=1
+                queue.append([temp_n-1,time+1])
+            if temp_n+1<=100000 and visited[temp_n+1]==0:
+                visited[temp_n+1]=1
+                queue.append([temp_n+1,time+1])
+            if temp_n*2<=100000 and visited[temp_n*2]==0:
+                visited[temp_n*2]=1
+                queue.append([temp_n*2,time+1])
