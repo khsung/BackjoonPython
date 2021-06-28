afdbc89a84f196746d4b5e36b85024241a5c79de
khsung <khsung0@nate.com>
2021-06-29 03:27:57 +0900

    숫자 카드2(시간 초과)

--- Backjoon_Python/BinarySearch/10816(Number Card2).py
+++ Backjoon_Python/BinarySearch/10816(Number Card2).py
@@ -1,29 +1,35 @@
-#10816 숫자 카드2 수정중
+#10816 숫자 카드2
 import sys
 n=int(input())
-n_arr=list(map(int,sys.stdin.readline().split()))
+first_array=list(map(int,sys.stdin.readline().split()))
+first_array.sort()
 m=int(input())
-m_arr=list(map(int,sys.stdin.readline().split()))
-n_arr.sort()
-n_arr_dic={}
-n_arr_index=0
-while n_arr_index<len(n_arr):
+second_array=list(map(int,sys.stdin.readline().split()))
+for i in range(m):
+    left=0
+    right=n-1
     cnt=0
-    for i in range(n_arr_index,len(n_arr)):
-        if i==n_arr_index:
-            cnt+=1
-        elif n_arr[i]!=n_arr[i-1]:
-            n_arr_index=i
-            n_arr_dic[n_arr[i-1]]=cnt
-            break
-        elif i==len(n_arr)-1:
-            n_arr_index=i+1
-            n_arr_dic[n_arr[i-1]]=cnt+1
-            break
+    check=True
+    while left<=right and check==True:
+        mid=(left+right)//2
+        if first_array[mid]==second_array[i]:
+            temp_dis=0
+            while True:
+                if mid>=0 and first_array[mid]==second_array[i]:
+                    mid-=1
+                    temp_dis+=1
+                else:
+                    temp_dis-=1
+                    mid+=1
+                    break
+            mid+=temp_dis
+            cnt+=temp_dis
+            while mid<n and first_array[mid]==second_array[i]:
+                cnt+=1
+                mid+=1
+            check=False
+        elif first_array[mid]<second_array[i]:
+            left=mid+1
         else:
-            cnt+=1
-for i in range(len(m_arr)):
-    if m_arr[i] in n_arr_dic:
-        print(n_arr_dic[m_arr[i]],end=" ")
-    else:
-        print(0,end=" ")
+            right=mid-1
+    print(cnt,end=" ")
