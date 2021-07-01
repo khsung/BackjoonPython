cfcbb5fbf351ab8a468cff88b3515445cfc23aee
khsung <khsung0@nate.com>
2021-07-01 12:51:16 +0900

    집합(메모리 초과)

--- Backjoon_Python/Implement/11723(Set).py
+++ Backjoon_Python/Implement/11723(Set).py
@@ -6,9 +6,11 @@ for i in range(n):
     op=sys.stdin.readline().split()
     if len(op)==1:
         if op[0]=='all':
-            S=[1 for i in range(20)]
+            for j in range(20):
+                S[j]=1
         else:
-            S=[0 for i in range(20)]
+            for j in range(20):
+                S[j]=0
     else:
         if op[0]=='add':
             S[int(op[1])-1]=1
