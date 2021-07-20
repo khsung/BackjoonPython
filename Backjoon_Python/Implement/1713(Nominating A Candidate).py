44d222811f17a06ad680c44724b53d0176ada50d
khsung <khsung0@nate.com>
2021-07-20 09:37:10 +0900

    후보 추천하기(합격)

--- Backjoon_Python/Implement/1713(Nominating A Candidate).py
+++ Backjoon_Python/Implement/1713(Nominating A Candidate).py
@@ -3,17 +3,12 @@ n=int(input())
 m=int(input())
 m_list=list(map(int,input().split()))
 pic=[]
+timer=1
 for i in range(len(m_list)):
     if len(pic)==0:
-        pic.append([m_list[i],1])
-    elif len(pic)<n:
-        check=True
-        for j in range(len(pic)):
-            if pic[j][0]==m_list[i]:
-                pic[j][1]+=1
-                check=False
-        if check:
-            pic.append([m_list[i],1])
+        #후보 번호, 추천 횟수, 추천 시기
+        pic.append([m_list[i],1,timer])
+        timer+=1
     else:
         check=True
         for j in range(len(pic)):
@@ -21,12 +16,13 @@ for i in range(len(m_list)):
                 pic[j][1]+=1
                 check=False
         if check:
-            pic.sort(key=lambda x:x[1])
-            temp=len(pic)-1
-            while temp-1>=0 and pic[temp][1]==pic[temp-1][1]:
-                temp-=1
-            del pic[temp]
-            pic.append([m_list[i],1])
+            if len(pic)<n:
+                pic.append([m_list[i],1,timer])
+                timer+=1
+            else:
+                pic.sort(key=lambda x:(-x[1],-x[2]))
+                pic[len(pic)-1]=[m_list[i],1,timer]
+                timer+=1
 pic.sort(key=lambda x:x[0])
 for i in range(len(pic)):
     print(pic[i][0],end=" ")
