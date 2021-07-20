c78a246b655af3e8d71f3c2239898edc770445b9
khsung <khsung0@nate.com>
2021-07-21 00:06:18 +0900

    게임(합격)

--- Backjoon_Python/Math/1072(Game).py
+++ Backjoon_Python/Math/1072(Game).py
@@ -1,8 +1,27 @@
 #1072 게임
-import math
+
+#수학적 풀이
+#import math
+#x,y=map(int,input().split())
+#z=(100*y)//x       파이썬의 실수오류 주의!!
+#if z>=99:
+#    print(-1)
+#else:
+#    print(math.ceil((x*z+x-100*y)/(99-z)))
+
 x,y=map(int,input().split())
-z=math.floor((y/x)*100)
+z=(100*y)//x
+left=0
+right=x
+res=x
 if z>=99:
     print(-1)
 else:
-    print(math.ceil((x*z+x-100*y)/(100-z-1)))
+    while left<=right:
+        mid=(left+right)//2
+        if (100*(y+mid))//(x+mid)>z:
+            res=mid
+            right=mid-1
+        else:
+            left=mid+1
+    print(res)
