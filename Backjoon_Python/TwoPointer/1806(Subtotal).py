7560016f5367aa3e6ef83b16a00960570e14a43f
khsung <khsung0@nate.com>
2021-07-20 13:44:21 +0900

    부분합(합격)

--- Backjoon_Python/TwoPointer/1806(Subtotal).py
+++ Backjoon_Python/TwoPointer/1806(Subtotal).py
@@ -1,24 +1,21 @@
 #1806 부분합
 n,s=map(int,input().split())
-sequence=list(map(int,input().split()))
-tempsum=0
-maxindexsize=100001
-start=end=0
-plusstart=True
-while start<n:
-    if plusstart==True:
-        tempsum+=sequence[start]
-    if tempsum<s:
-        start+=1
-        plusstart=True
+left=-1
+right=0
+min_len=100001
+num_array=list(map(int,input().split()))
+temp_sum=num_array[0]
+while right<n:
+    if temp_sum>=s:
+        if min_len>right-left:
+            min_len=right-left
+        left+=1
+        temp_sum-=num_array[left]
     else:
-        if start-end+1<maxindexsize:
-            maxindexsize=start-end+1
-        tempsum-=sequence[end]
-        end+=1
-        plusstart=False
-
-if maxindexsize==100001:
+        right+=1
+        if right<n:
+            temp_sum+=num_array[right]
+if min_len==100001:
     print(0)
 else:
-    print(maxindexsize)
+    print(min_len)
