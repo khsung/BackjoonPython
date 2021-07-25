51138ff85ea1b3ed9acc6781b99b6f9841f7284a
khsung <khsung0@nate.com>
2021-07-25 21:55:01 +0900

    N-Queen(시간초과)

--- Backjoon_Python/Backtracking/9663(N-Queen).py
+++ Backjoon_Python/Backtracking/9663(N-Queen).py
@@ -1,27 +1,36 @@
 #9663 N-Queen (시간초과)
+n=int(input())
 
-import time
-num=int(input())
-start = time.time()
-nqueen=[]
-count=0
-def linechk(nqueen,row,col):
-    for i in range(len(nqueen)):
-        if (nqueen[i][1]==col) or abs(nqueen[i][0]-row)==abs(nqueen[i][1]-col):
-            return 0
-    return 1
-def Nqueen(nqueen,cur,maxline):
-    global count
-    if cur==maxline:
-        for i in range(1,maxline+1):
-            if linechk(nqueen,cur,i):
-                count+=1
+queen=[]
+cnt=0
+def check_queen(currx,curry):
+    global queen
+    check=True
+    for i in queen:
+        if i[0]==currx or abs(currx-i[0])==abs(curry-i[1]):
+            check=False
+            break
+    if check:
+        return True
     else:
-        for i in range(1,maxline+1):
-            if linechk(nqueen,cur,i):
-                nqueen.append([cur,i])
-                Nqueen(nqueen,cur+1,maxline)
-                nqueen.pop()
-Nqueen(nqueen,1,num)
-print(count)
-print("time :", time.time() - start)
+        return False
+
+def find_path(currx,curry):
+    global n,cnt
+    if curry<n-1:
+        for i in range(n):
+            if check_queen(i,curry):
+                
+                queen.append([i,curry])
+                find_path(i,curry+1)
+                queen.pop()
+    else:
+        for i in range(n):
+            if check_queen(i,curry):
+                cnt+=1
+
+for i in range(n):
+    queen.append([i,0])
+    find_path(i,1)
+    queen.pop()
+print(cnt)
