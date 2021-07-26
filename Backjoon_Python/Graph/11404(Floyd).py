5cd590fa3045c2f9e1045c417fc7082a917604bc
khsung <khsung0@nate.com>
2021-07-26 19:12:55 +0900

    플로이드(시간초과)

--- Backjoon_Python/Graph/11404(Floyd).py
+++ Backjoon_Python/Graph/11404(Floyd).py
@@ -1,33 +1,28 @@
-#11404 플로이드 미완성
+#11404 플로이드
 n=int(input())
-graph=[[100000 for i in range(n)]for j in range(n)]
+graph=[[100001 for i in range(n)]for j in range(n)]
 m=int(input())
 for i in range(m):
     a,b,c=map(int,input().split())
-    if graph[a-1][b-1]==0:
+    if graph[a-1][b-1]==0 or graph[a-1][b-1]>c:
         graph[a-1][b-1]=c
-    else:
-        if graph[a-1][b-1]>c:
-            graph[a-1][b-1]=c
 
-for i in range(len(graph)):
-    for j in range(len(graph)):
-        print(graph[i][j],end=" ")
-    print()
+for i in range(n):
+    for j in range(n):
+        for k in range(n):
+            if graph[i][j]+graph[j][k]<graph[i][k]:
+                graph[i][k]=graph[i][j]+graph[j][k]
+            if i==k:
+                graph[i][k]=0
 
-print("==============")
+for i in range(n):
+    for j in range(n):
+        for k in range(n):
+            if graph[i][j]+graph[j][k]<graph[i][k]:
+                graph[i][k]=graph[i][j]+graph[j][k]
+            if i==k:
+                graph[i][k]=0
 
-#이부분 문제
-for i in range(len(graph)):
-    for j in range(len(graph)):
-        for k in range(len(graph)):
-            if graph[j][i]!=0 and graph[i][k]!=0 and graph[j][k]!=0 and graph[j][i]+graph[i][k]<graph[j][k]:
-                print(i,j,k,graph[j][i]+graph[i][k],graph[j][k])
-                graph[j][k]=graph[j][i]+graph[i][k]
-            
-
-
-print("==============")
 for i in range(len(graph)):
     for j in range(len(graph)):
         print(graph[i][j],end=" ")
