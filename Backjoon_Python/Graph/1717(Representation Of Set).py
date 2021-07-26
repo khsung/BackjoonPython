86040330c84136cb42208fa3444de45150fe2596
khsung <khsung0@nate.com>
2021-07-26 10:45:11 +0900

    집합의 표현(불합격)

--- Backjoon_Python/Graph/1717(Representation Of Set).py
+++ Backjoon_Python/Graph/1717(Representation Of Set).py
@@ -1,26 +1,26 @@
 #1717 집합의 표현
-n,m=map(int,input().split())
+import sys
+sys.setrecursionlimit(10**9)
+n,m=map(int,sys.stdin.readline().split())
 union_find=[i for i in range(n+1)]
-for i in range(m):
-    op,a,b=map(int,input().split())
-    if a>b:
-        a,b=b,a
-    if op==0:
-        parenta=union_find[a]
-        parentb=union_find[b]
-        if parenta<parentb:
-            while union_find.count(parentb)!=0:
-                union_find[union_find.index(parentb)]=parenta
-        elif parenta>parentb:
-            parent=parentb
-            while union_find.count(parentb)!=0:
-                union_find[union_find.index(parenta)]=parentb
+print(union_find)
+def find(a):
+    global union_find
+    if union_find[a]==a:
+        return a
     else:
-        while a!=union_find[a]:
-            a=union_find[a]
-        while b!=union_find[b]:
-            b=union_find[b]
-        if a==b:
+        union_find[a]=find(union_find[a])
+        return union_find[a]
+
+for i in range(m):
+    temp=list(map(int,sys.stdin.readline().split()))
+    if temp[0]==0:
+        if temp[1]<temp[2]:
+            temp[1],temp[2]=temp[2],temp[1]
+        union_find[temp[1]]=temp[2]
+    else:
+        if find(temp[1])==find(temp[2]):
             print("YES")
         else:
             print("NO")
+    print(union_find)
