d91b97cfd291d97c1e7536d24ce8ee1d97e6f7b7
khsung <khsung0@nate.com>
2021-07-26 11:29:29 +0900

    집합의 표현(합격)

--- Backjoon_Python/Graph/1717(Representation Of Set).py
+++ Backjoon_Python/Graph/1717(Representation Of Set).py
@@ -3,7 +3,6 @@ import sys
 sys.setrecursionlimit(10**9)
 n,m=map(int,sys.stdin.readline().split())
 union_find=[i for i in range(n+1)]
-print(union_find)
 def find(a):
     global union_find
     if union_find[a]==a:
@@ -17,10 +16,9 @@ for i in range(m):
     if temp[0]==0:
         if temp[1]<temp[2]:
             temp[1],temp[2]=temp[2],temp[1]
-        union_find[temp[1]]=temp[2]
+        union_find[find(temp[1])]=find(temp[2])
     else:
         if find(temp[1])==find(temp[2]):
             print("YES")
         else:
             print("NO")
-    print(union_find)
