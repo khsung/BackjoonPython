c3855a214121b042da2704238f4001278e31d7b1
khsung <khsung0@nate.com>
2021-07-31 19:34:47 +0900

    이진 검색 트리(불합격)

--- Backjoon_Python/Tree/5639(Binary Search Tree).py
+++ Backjoon_Python/Tree/5639(Binary Search Tree).py
@@ -1,40 +1,28 @@
 #5639 이진 검색 트리
-from collections import deque
-import sys
-sys.setrecursionlimit(10 ** 8)
-
-def postorder(index):
-    global tree
-    if tree[2*index]!=0:
-        postorder(2*index)
-    if tree[2*index+1]!=0:
-        postorder(2*index+1)
-    print(tree[index])
-
-tree=[0 for i in range(500000)]
-data=deque()
+queue=[]
+dic={}
+cnt=0
 while True:
     try:
-        data.append(int(input()))
+        cnt+=1
+        temp=int(input())
+        
+        if cnt==1:
+            dic[queue[0]]=[0,0]
+            root=queue[0]
+            queue.append(temp)
+            parent=temp
+        else:
+            if temp<root:
+                if parent>temp:
+                    dic[parent][0]=temp
+                    queue.append(temp)
+                    parent=temp
+                else:
+                    while queue[len(queue)-2]<temp:
+                        queue.pop()
+                    dic[queue.pop()][1]=temp
+        print(dic)
+
     except:
         break
-
-if len(data)>0:
-    prev_stack=[]
-    prev_stack.append([data.popleft(),1])
-    tree[1]=prev_stack[0][0]
-    curr_index=1
-    while len(data)>0:
-        temp=data.popleft()
-        if len(prev_stack)>0 and prev_stack[len(prev_stack)-1][0]>temp:
-            curr_index=curr_index*2
-            tree[curr_index]=temp
-            prev_stack.append([temp,curr_index])
-        else:
-            while len(prev_stack)>0 and prev_stack[len(prev_stack)-1][0]<temp:
-                temp1=prev_stack.pop()
-            curr_index=temp1[1]*2+1
-            tree[curr_index]=temp
-            prev_stack.append([temp,curr_index])
-
-    postorder(1)
