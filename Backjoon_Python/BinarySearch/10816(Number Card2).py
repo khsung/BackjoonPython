b626138f9cd652047dd8a6861f72bca60a5ba0e7
khsung <khsung0@nate.com>
2021-08-04 17:23:37 +0900

    숫자 카드2(합격)

--- Backjoon_Python/BinarySearch/10816(Number Card2).py
+++ Backjoon_Python/BinarySearch/10816(Number Card2).py
@@ -1,21 +1,46 @@
 #10816 숫자 카드2
 import sys
 n=int(input())
-first_array=list(map(int,sys.stdin.readline().split()))
-first_array.sort()
+n_list=list(map(int,sys.stdin.readline().split()))
 m=int(input())
-second_array=list(map(int,sys.stdin.readline().split()))
-for i in second_array:
+m_list=list(map(int,sys.stdin.readline().split()))
+n_list.sort()
+for i in range(m):
     left=0
     right=n-1
-    cnt=0
+    target=-1
     while left<=right:
         mid=(left+right)//2
-        if first_array[mid]==i:
-            cnt=first_array[left:right+1].count(i)
+        if n_list[mid]==m_list[i]:
+            target=mid
             break
-        elif first_array[mid]<i:
+        elif n_list[mid]<m_list[i]:
             left=mid+1
         else:
             right=mid-1
-    print(cnt,end=" ")
+    if target==-1:
+        print(0,end=" ")
+    else:
+        min_target=max_target=target
+        left=0
+        right=min_target
+        while left<=right:
+            mid=(left+right)//2
+            if n_list[mid]==m_list[i]:
+                if min_target>mid:
+                    min_target=mid
+                right=mid-1
+            else:
+                left=mid+1
+        left=max_target
+        right=n-1
+        while left<=right:
+            mid=(left+right)//2
+            if n_list[mid]==m_list[i]:
+                if mid>max_target:
+                    max_target=mid
+                left=mid+1
+            else:
+                right=mid-1
+
+        print(max_target-min_target+1,end=" ")
