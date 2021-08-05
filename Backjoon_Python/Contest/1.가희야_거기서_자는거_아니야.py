b4f645dccef99ad65c7ae5e6f2dfdf77429fd621
khsung <khsung0@nate.com>
2021-08-05 15:57:26 +0900

    가희야 거기서 자는거 아니야(합격)

--- Backjoon_Python/Contest/1.가희야_거기서_자는거_아니야.py
+++ Backjoon_Python/Contest/1.가희야_거기서_자는거_아니야.py
@@ -1,44 +1,11 @@
 # 1번 가희야 거기서 자는거 아니야
-R,C=input().split()
-R=int(R)
-C=int(C)
-Rg,Cg,Rp,Cp=input().split()
-Rg=int(Rg)
-Cg=int(Cg)
-Rp=int(Rp)
-Cp=int(Cp)
-min_Rg=R
-min_Cg=C
-min_Rp=R
-min_Cp=C
-graph=[]
-for i in range(R):
-    temp_graph=list(input())
-    graph.append(temp_graph)
-for i in range(R):
-    for j in range(C):
-        if graph[i][j]=="G":
-            if i<min_Rg:
-                min_Rg=i
-            elif j<min_Cg:
-                min_Cg=j
-        if graph[i][j]=="P":
-            if i<min_Rp:
-                min_Rp=i
-            elif j<min_Cp:
-                min_Cp=j
-if min_Rg+Rg<min_Rp or min_Rg>min_Rp+Rp or min_Cg+Cg<min_Cp or min_Cg>min_Cp+Cp:
+r,c=map(int,input().split())
+r_g,c_g,r_p,c_p=map(int,input().split())
+cnt_sum=0
+for i in range(r):
+    temp=list(input())
+    cnt_sum+=temp.count("P")
+if cnt_sum==r_p*c_p:
     print(0)
 else:
-    if min_Rg<min_Rp:
-        min_R=min_Rp
-    else:
-        min_R=min_Rg
-    if min_Cg<min_Cp:
-        min_C=min_Cp
-    else:
-        min_C=min_Cg
-    if graph[min_R][min_C]=="G":
-        print(1)
-    else:
-        print(0)
+    print(1)
