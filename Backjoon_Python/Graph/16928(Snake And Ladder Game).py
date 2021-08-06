a8b178e3a9ce1b4225f503f900f0a63adcdf45ab
khsung <khsung0@nate.com>
2021-08-06 19:20:56 +0900

    뱀과 사다리 게임(합격)

--- Backjoon_Python/Graph/16928(Snake And Ladder Game).py
+++ Backjoon_Python/Graph/16928(Snake And Ladder Game).py
@@ -2,22 +2,14 @@
 from collections import deque
 edge_info=[0 for i in range(101)]
 n,m=map(int,input().split())
-visited=[0 for i in range(101)]
-visited[1]=1
-for i in range(n+m):
-    start,end=map(int,input().split())
-    edge_info[start]=end
-    
+visited=[0 for i in range(100)]
 queue=deque()
+for i in range(n+m):
+    temp=list(map(int,input().split()))
+    edge_info[temp[0]]=temp[1]
 
-if edge_info[1]==0:
-    queue.append([1,0])
-else:
-    start=1
-    while edge_info[start]!=0 and start<edge_info[start]:
-        start=edge_info[start]
-    queue.append([start,0])
-
+             #curr,cnt
+queue.append([1,0])
 while len(queue)>0:
     curr,cnt=queue.popleft()
     if curr==100:
@@ -28,11 +20,11 @@ while len(queue)>0:
         break
     else:
         for i in range(1,7):
-            if visited[curr+i]==0:
-                if edge_info[curr+i]==0:
-                    queue.append([curr+i,cnt+1])
-                    visited[curr+i]=1
-                elif curr+i<edge_info[curr+i]:
-                    queue.append([edge_info[curr+i],cnt+1])
+            if edge_info[curr+i]==0 and visited[curr+i]==0:
+                visited[curr+i]=1
+                queue.append([curr+i,cnt+1])
+            else:
+                if visited[curr+i]==0:
                     visited[curr+i]=1
                     visited[edge_info[curr+i]]=1
+                    queue.append([edge_info[curr+i],cnt+1])
