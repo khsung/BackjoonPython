91956cef1a0897270f40909ee848b2d7a614430e
khsung <khsung0@nate.com>
2021-08-11 19:12:24 +0900

    최단경로(시간초과)

--- Backjoon_Python/Graph/1753(Shortest Path).py
+++ Backjoon_Python/Graph/1753(Shortest Path).py
@@ -1,18 +1,34 @@
-#1753 최단경로 미완성
+#1753 최단경로
+import sys
 v,e=map(int,input().split())
 INF=float('inf')
-graph=[[INF for i in range(v)]for j in range(v)]
-startnode=int(input())
-path=[INF for i in range(v)]
+edge_list=[[] for i in range(v)]
+startnode=int(input())-1
+cost=[INF for i in range(v)]
 visited=[0 for i in range(v)]
+visited[startnode]=1
+cost[startnode]=0
 for i in range(e):
-    u,v,w=map(int, input().split())
-    graph[u-1][v-1]=w
+    temp=list(map(int,sys.stdin.readline().split()))
+    edge_list[temp[0]-1].append([temp[1]-1,temp[2]])
 
-
-
-
-
-for i in range(len(graph)):
-    print(graph[i])
-print("visited =",visited)
+while True:
+    for i in range(len(edge_list[startnode])):
+        if cost[edge_list[startnode][i][0]]>edge_list[startnode][i][1]+cost[startnode]:
+            cost[edge_list[startnode][i][0]]=edge_list[startnode][i][1]+cost[startnode]
+    temp_index=INF
+    min_cost=INF
+    for i in range(v):
+        if visited[i]==0 and cost[i]<min_cost:
+            min_cost=cost[i]
+            temp_index=i
+    if temp_index==INF:
+        break
+    else:
+        visited[temp_index]=1
+        startnode=temp_index
+for i in cost:
+    if i==INF:
+        print("INF")
+    else:
+        print(i)
