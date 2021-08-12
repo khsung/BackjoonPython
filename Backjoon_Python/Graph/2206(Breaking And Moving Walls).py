407b33a103066e7c81aa49598c0326eb14dbfd42
khsung <khsung0@nate.com>
2021-08-13 00:10:38 +0900

    벽 부수고 이동하기(합격)

--- Backjoon_Python/Graph/2206(Breaking And Moving Walls).py
+++ Backjoon_Python/Graph/2206(Breaking And Moving Walls).py
@@ -1,52 +1,41 @@
 #2206 벽 부수고 이동하기
+import sys
+from collections import deque
+
+#위,오,아,왼
+dx=[-1,0,1,0]
+dy=[0,1,0,-1]
 n,m=map(int,input().split())
-square=[]
-visited=[[0 for i in range(m)]for j in range(n)]
-currposi=[]
-res=[]
+graph=[]
+res=-1
+queue=deque()
 for i in range(n):
-    temp=list(map(int,input()))
-    square.append(temp)
-currposi.append([0,0,False])
-cnt=0
-while len(currposi)>0:
-    cnt+=1
-    size=len(currposi)
-    for i in range(size):
-        x=currposi[0][0]
-        y=currposi[0][1]
-        wall=currposi[0][2]
-        currposi.pop(0)
-        visited[x][y]=1
-        if x==n-1 and y==m-1:
-            res.append(cnt)
-            break
-        else:
-            if x-1>=0 and visited[x-1][y]==0:
-                if square[x-1][y]==0:
-                    currposi.append([x-1,y,wall])
-                else:
-                    if wall==False:
-                        currposi.append([x-1,y,True])
-            if y+1<m and visited[x][y+1]==0:
-                if square[x][y+1]==0:
-                    currposi.append([x,y+1,wall])
-                else:
-                    if wall==False:
-                        currposi.append([x,y+1,True])
-            if x+1<n and visited[x+1][y]==0:
-                if square[x+1][y]==0:
-                    currposi.append([x+1,y,wall])
-                else:
-                    if wall==False:
-                        currposi.append([x+1,y,True])
-            if y-1>=0 and visited[x][y-1]==0:
-                if square[x][y-1]==0:
-                    currposi.append([x,y-1,wall])
-                else:
-                    if wall==False:
-                        currposi.append([x,y-1,True])
-if len(res)==0:
-    print(-1)
+    graph.append(list(map(int,list(sys.stdin.readline().rstrip()))))
+visited=[[[0 for i in range(m)] for j in range(n)]for k in range(2)]
+
+       #벽 부수기 전,시작위치,거리
+queue.append([0,0,0,1])
+visited[0][0][0]=1
+while len(queue)>0:
+    temp_list=queue.popleft()
+    for i in range(4):
+        x=temp_list[1]+dx[i]
+        y=temp_list[2]+dy[i]
+        if 0<=x<n and 0<=y<m:
+            if x==n-1 and y==m-1:
+                res=temp_list[3]+1
+                break
+            elif graph[x][y]==0 and visited[temp_list[0]][x][y]==0:
+                visited[temp_list[0]][x][y]=1
+                queue.append([temp_list[0],x,y,temp_list[3]+1])
+            elif graph[x][y]==1:
+                if temp_list[0]==0 and visited[0][x][y]==0:
+                    visited[1][x][y]=1
+                    queue.append([temp_list[0]+1,x,y,temp_list[3]+1])
+
+    if res!=-1:
+        break
+if n==1 and m==1:
+    print(1)
 else:
-    print(min(res))
+    print(res)
