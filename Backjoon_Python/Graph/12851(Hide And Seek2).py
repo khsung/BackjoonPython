8dbf3a5ce507de3a2dffac90410f5d8480931391
khsung <khsung0@nate.com>
2021-08-14 18:04:08 +0900

    숨바꼭질2(불합격)

--- Backjoon_Python/Graph/12851(Hide And Seek2).py
+++ Backjoon_Python/Graph/12851(Hide And Seek2).py
@@ -1,41 +1,59 @@
 #12851 숨바꼭질2
 from collections import deque
-n,k=map(int,input().split())
-visited=[0 for i in range(100009)]
-visited[n]=1
-min_time=100009
-cnt=0
 queue=deque()
+res=1000000
+res_cnt=0
+check=False
+n,k=map(int,input().split())
+visited=[0 for i in range(100001)]
 if n==k:
     print(0)
-    print(cnt)
 else:
     queue.append([n,0])
+    visited[n]=1
     while len(queue)>0:
-        curr,curr_time=queue.popleft()
-        if curr==k:
-            if min_time>curr_time:
-                min_time=curr_time
-                cnt+=1
-            elif min_time==curr_time:
-                cnt+=1
+        curr,cnt=queue.popleft()
+        if cnt+1>res:
+            break
+        if curr-1>=0 and visited[curr-1]<=2:
+            if curr-1==k:
+                if res>cnt+1:
+                    res=cnt+1
+                    res_cnt=1
+                elif res==cnt+1:
+                    res_cnt+=1
+                else:
+                    check=True
             else:
-                break
-        else:
-            if min_time!=100009 and min_time<curr_time:
-                break
+                queue.append([curr-1,cnt+1])
+                visited[curr-1]+=1
+        if curr+1<=k and visited[curr+1]<=2:
+            if curr+1==k:
+                if res>cnt+1:
+                    res=cnt+1
+                    res_cnt=1
+                elif res==cnt+1:
+                    res_cnt+=1
+                else:
+                    check=True
             else:
-                if curr+1<=100000 and visited[curr+1]==0 and curr<k:
-                    if curr+1!=k:
-                        visited[curr+1]=1
-                    queue.append([curr+1,curr_time+1])
-                if curr-1>=0 and visited[curr-1]==0:
-                    if curr-1!=k:
-                        visited[curr-1]=1
-                    queue.append([curr-1,curr_time+1])
-                if 2*curr<=100000 and visited[2*curr]==0 and curr<k:
-                    if 2*curr!=k:
-                        visited[2*curr]=1
-                    queue.append([2*curr,curr_time+1])
-    print(min_time)
-    print(cnt)
+                visited[curr+1]+=1
+                queue.append([curr+1,cnt+1])
+
+        if curr*2<=100000 and visited[curr*2]<=2:
+            if curr*2==k:
+                if res>cnt+1:
+                    res=cnt+1
+                    res_cnt=1
+                elif res==cnt+1:
+                    res_cnt+=1
+                else:
+                    check=True
+            else:
+                visited[curr*2]+=1
+                queue.append([curr*2,cnt+1])
+
+        if check:
+            break
+    print(res)
+    print(res_cnt)
