ef9942ad986be93e4ac1e080ea7ce26a31e6e9c7
khsung <khsung0@nate.com>
2021-08-16 18:15:30 +0900

    최단경로(시간초과)

--- Backjoon_Python/Graph/1753(Shortest Path).py
+++ Backjoon_Python/Graph/1753(Shortest Path).py
@@ -1,34 +1,32 @@
 #1753 최단경로
 import sys
-v,e=map(int,input().split())
 INF=float('inf')
-edge_list=[[] for i in range(v)]
-startnode=int(input())-1
-cost=[INF for i in range(v)]
+v,e=map(int,input().split())
+start=int(input())-1
 visited=[0 for i in range(v)]
-visited[startnode]=1
-cost[startnode]=0
+cost=[INF for i in range(v)]
+path=[[]for j in range(v)]
+cost[start]=0
+visited[start]=1
 for i in range(e):
     temp=list(map(int,sys.stdin.readline().split()))
-    edge_list[temp[0]-1].append([temp[1]-1,temp[2]])
+    path[temp[0]-1].append([temp[1]-1,temp[2]])
 
-while True:
-    for i in range(len(edge_list[startnode])):
-        if cost[edge_list[startnode][i][0]]>edge_list[startnode][i][1]+cost[startnode]:
-            cost[edge_list[startnode][i][0]]=edge_list[startnode][i][1]+cost[startnode]
-    temp_index=INF
-    min_cost=INF
-    for i in range(v):
-        if visited[i]==0 and cost[i]<min_cost:
-            min_cost=cost[i]
-            temp_index=i
-    if temp_index==INF:
-        break
-    else:
-        visited[temp_index]=1
-        startnode=temp_index
-for i in cost:
-    if i==INF:
+for i in range(v):
+    for j in range(len(path[start])):
+        if path[start][j][1]+cost[start]<cost[path[start][j][0]]:
+            cost[path[start][j][0]]=path[start][j][1]+cost[start]
+
+    temp_cost=INF
+    for j in range(v):
+        if visited[j]==0 and cost[j]<temp_cost:
+            start=j
+            temp_cost=cost[j]
+
+    visited[start]=1
+
+for i in range(v):
+    if cost[i]==INF:
         print("INF")
     else:
-        print(i)
+        print(cost[i])
