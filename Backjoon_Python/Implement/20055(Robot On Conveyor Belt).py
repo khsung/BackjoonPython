2693b2fad8033e67bae3d9f2c8f4116295dc9bf2
khsung <khsung0@nate.com>
2021-08-18 17:40:22 +0900

    컨베이어 벨트 위의 로봇(불합격)

---
+++ Backjoon_Python/Implement/20055(Robot On Conveyor Belt).py
@@ -0,0 +1,30 @@
+#20055 컨베이어 벨트 위의 로봇
+from collections import deque
+n,k=map(int,input().split())
+cnt_zero=0
+res=0
+temp_belt=list(map(int,input().split()))
+belt=deque()
+for i in temp_belt:
+                #내구도, 로봇
+    belt.append([i,0])
+while True:
+    res+=1
+    temp=belt.pop()
+    belt.appendleft(temp)
+    belt[n-1][1]=0
+    for i in range(n-2,-1,-1):
+        if belt[i][1]!=0 and belt[i+1][1]==0 and belt[i+1][0]>0:
+            belt[i][1],belt[i+1][1]=belt[i+1][1],belt[i][1]
+            belt[i+1][0]-=1
+            if belt[i+1][0]==0:
+                cnt_zero+=1
+
+    if belt[0][0]!=0:
+        belt[0][0]-=1
+        belt[0][1]=1
+        if belt[0][0]==0:
+            cnt_zero+=1
+    if cnt_zero>=k:
+        break
+print(res)
