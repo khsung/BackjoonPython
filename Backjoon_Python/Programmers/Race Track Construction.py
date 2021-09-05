f83865d75978f86e0071f6c97f22b51c96b99cef
khsung <khsung0@nate.com>
2021-09-05 18:51:52 +0900

    경주로 건설(불합격)

--- Backjoon_Python/Programmers/Race Track Construction.py
+++ Backjoon_Python/Programmers/Race Track Construction.py
@@ -3,41 +3,42 @@ from collections import deque
 import copy
 def solution(board):
     answer = 100000
+    board_cost=[[100000 for i in range(len(board[0]))] for j in range(len(board))]
     dir=[[-1,0],[0,1],[1,0],[0,-1]]
     queue=deque()
-    queue.append([[[0,0]],0])
+    if board[0][1]==0:
+        queue.append([[[0,0],[0,1]],100])
+        board_cost[0][1]=100
+    if board[1][0]==0:
+        queue.append([[[0,0],[1,0]],100])
+        board_cost[1][0]=100
     while len(queue)>0:
-        print(queue)
-        temp=queue.popleft()
-        for i in range(4):
-            print(temp[0][len(temp[0])-1][0])
-            x=temp[0][len(temp[0])-1][0]+dir[i][0]
-            y=temp[0][len(temp[0])-1][1]+dir[i][1]
-            if 0<=x<len(board) and 0<=y<len(board[0]) and board[x][y]==0:
-                if len(temp[0])==1:
-                    temp=copy.deepcopy(temp[0])
-                    temp.append([x,y])
-                    queue.append([temp,100])
+        path,cost=queue.popleft()
+        for i in dir:
+            x=path[len(path)-1][0]+i[0]
+            y=path[len(path)-1][1]+i[1]
+            if 0<=x<len(board) and 0<=y<len(board[0]) and board[x][y]==0 and [x,y] not in path:
+                #직선일때
+                if i[0]==path[len(path)-1][0]-path[len(path)-2][0] and i[1]==path[len(path)-1][1]-path[len(path)-2][1]:
+                    if x==len(board)-1 and y==len(board[0])-1:
+                        if answer>cost+100:
+                            answer=cost+100
+                    else:
+                        if answer>cost+100 and board_cost[x][y]>=cost+100:
+                            board_cost[x][y]=cost+100
+                            temp_path=copy.deepcopy(path)
+                            temp_path.append([x,y])
+                            queue.append([temp_path,cost+100])
                 else:
-                    if [x,y] not in temp[0]:
-                        temp_cost=temp[1]
-                        #print(temp[0][len(temp[0])-1][0]-temp[0][len(temp[0])-2][0],x-temp[0][len(temp[0])-1][0],temp[0][len(temp[0])-1][1]-temp[0][len(temp[0])-2][1],y-temp[0][len(temp[0])-1][1])
-                        if (temp[0][len(temp[0])-1][0]-temp[0][len(temp[0])-2][0])==(x-temp[0][len(temp[0])-1][0]) and (temp[0][len(temp[0])-1][1]-temp[0][len(temp[0])-2][1])==(y-temp[0][len(temp[0])-1][1]):
-                            #print(temp[0][len(temp[0])-1],temp[0][len(temp[0])-2],x,y)
-                            #print(temp[0][len(temp[0])-1][0]-temp[0][len(temp[0])-2][0],x-temp[0][len(temp[0])-1][0],temp[0][len(temp[0])-1][1]-temp[0][len(temp[0])-2][1],y-temp[0][len(temp[0])-1][1])
-                            temp_cost+=100
-                        else:
-                            temp_cost+=500
-                        if x==len(board)-1 and y==len(board[0])-1:
-                            if answer>temp_cost:
-                                answer=temp_cost
-                                temp=copy.deepcopy(temp[0])
-                                temp.append([x,y])
-                                print(temp)
-                        else:
-                            temp=copy.deepcopy(temp[0])
-                            temp.append([x,y])
-                            queue.append([temp,temp_cost])
+                    if x==len(board)-1 and y==len(board[0])-1:
+                        if answer>cost+600:
+                            answer=cost+600
+                    else:
+                        if answer>cost+600 and board_cost[x][y]>=cost+600:
+                            board_cost[x][y]=cost+600
+                            temp_path=copy.deepcopy(path)
+                            temp_path.append([x,y])
+                            queue.append([temp_path,cost+600])
     return answer
 
 board=[[[0,0,0],[0,0,0],[0,0,0]],[[0,0,0,0,0,0,0,1],[0,0,0,0,0,0,0,0],[0,0,0,0,0,1,0,0],[0,0,0,0,1,0,0,0],[0,0,0,1,0,0,0,1],[0,0,1,0,0,0,1,0],[0,1,0,0,0,1,0,0],[1,0,0,0,0,0,0,0]],[[0,0,1,0],[0,0,0,0],[0,1,0,1],[1,0,0,0]],[[0,0,0,0,0,0],[0,1,1,1,1,0],[0,0,1,0,0,0],[1,0,0,1,0,1],[0,1,0,0,0,1],[0,0,0,0,0,0]]]
