66d52dbe552fa8006fc7453704cf0646078a0847
khsung <khsung0@nate.com>
2021-09-10 04:20:51 +0900

    불량 사용자(합격)

--- Backjoon_Python/Programmers/Bad User.py
+++ Backjoon_Python/Programmers/Bad User.py
@@ -1,40 +1,47 @@
 #불량 사용자
 import copy
-def solution(user_id, banned_id):
+def backtracking(user_id, banned_id,curr_list):
     global answer,answer_list
-    answer = 0
-    answer_list=[]
-    backtracking(user_id, banned_id,0,[])
-    return answer
-
-def backtracking(user_id, banned_id,curr_index,result):
-    global answer,answer_list
-    print(result,curr_index)
-    if curr_index==len(banned_id):
-        result.sort()
-        answer_list.sort()
-        if result not in answer_list:
-            answer+=1
-            temp=copy.deepcopy(result)
+    if len(curr_list)==len(banned_id):
+        temp=copy.deepcopy(curr_list)
+        temp.sort()
+        if temp not in answer_list:
+            temp.sort()
             answer_list.append(temp)
-            
+            answer+=1
     else:
-        for i in user_id:
-            if len(i)==len(banned_id[curr_index]) and i not in result:
+        for i in range(len(user_id)):
+            if len(user_id[i])==len(banned_id[len(curr_list)]) and user_id[i] not in curr_list: 
                 check=True
-                for j in range(len(i)):
-                    if banned_id[curr_index][j]!="*" and banned_id[curr_index][j]!=i[j]:
+                for j in range(len(user_id[i])):
+                    if banned_id[len(curr_list)][j]!="*" and user_id[i][j]!=banned_id[len(curr_list)][j]:
                         check=False
                         break
                 if check:
-                    result.append(i)
-                    backtracking(user_id, banned_id,curr_index+1,result)
-                    result.pop()
+                    curr_list.append(user_id[i])
+                    backtracking(user_id, banned_id,curr_list)
+                    curr_list.pop()
+
+def solution(user_id, banned_id):
+    global answer_list,answer
+    answer = 0
+    answer_list=[]
+    curr_list=[]
+    for i in range(len(user_id)):
+        if len(user_id[i])==len(banned_id[0]):
+            check=True
+            for j in range(len(user_id[i])):
+                if banned_id[0][j]!="*" and  user_id[i][j]!=banned_id[0][j]:
+                    check=False
+                    break
+            if check:
+                curr_list.append(user_id[i])
+                backtracking(user_id, banned_id,curr_list)
+                curr_list.pop()
+
+    return answer
     
-    
-#user_id=[["frodo", "fradi", "crodo", "abc123", "frodoc"],["frodo", "fradi", "crodo", "abc123", "frodoc"],["frodo", "fradi", "crodo", "abc123", "frodoc"]]
-#banned_id=[["fr*d*", "abc1**"],["*rodo", "*rodo", "******"],["fr*d*", "*rodo", "******", "******"]]
-user_id=[["frodo", "fradi", "crodo", "abc123", "frodoc"]]
-banned_id=[["fr*d*", "*rodo", "******", "******"]]
+user_id=[["frodo", "fradi", "crodo", "abc123", "frodoc"],["frodo", "fradi", "crodo", "abc123", "frodoc"],["frodo", "fradi", "crodo", "abc123", "frodoc"]]
+banned_id=[["fr*d*", "abc1**"],["*rodo", "*rodo", "******"],["fr*d*", "*rodo", "******", "******"]]
 for i in range(len(user_id)):
     print(solution(user_id[i], banned_id[i]))
