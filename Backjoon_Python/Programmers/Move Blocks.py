e758b56e4f1292b85551d7a07c08ac02836b5ac3
khsung <khsung0@nate.com>
2021-09-09 23:06:33 +0900

    블록 이동하기(불합격)

--- Backjoon_Python/Programmers/Move Blocks.py
+++ Backjoon_Python/Programmers/Move Blocks.py
@@ -1,35 +1,58 @@
 #블록 이동하기
 from collections import deque
 def solution(board):
-    answer = 110000
+    answer=0
     dir=[[-1,0],[0,1],[1,0],[0,-1]]
-    visited=[[0 for i in range(len(board[0]))]for j in range(len(board))]
-    visited[0][0]=1
-    visited[0][1]=1
-
     queue=deque()
     queue.append([[[0,0],[0,1]],0])
-    while len(queue)>0:
+    robot={}
+    while len(queue)>0 and answer==0:
         curr,cnt=queue.popleft()
+        robot["".join(map(str,curr[0]))+"".join(map(str,curr[1]))]=1
+        robot["".join(map(str,curr[1]))+"".join(map(str,curr[0]))]=1
         for i in range(len(curr)):
-            for j in range(len(dir)):
-                x=curr[i][0]+dir[j][0]
-                y=curr[i][1]+dir[j][1]
-                if 0<=x<len(board) and 0<=y<len(board[0]) and board[x][y]==0 and curr[(i+1)%2]!=[x,y] and visited[x][y]<10:
-                    if board[curr[(i+1)%2][0]+dir[j][0]][curr[(i+1)%2][1]+dir[j][1]]==0:
-                        if x==len(board)-1 and y==len(board[0])-1:
-                            if answer>cnt+1:
+            for j in dir:
+                x=curr[i][0]+j[0]
+                y=curr[i][1]+j[1]
+                if 0<=x<len(board) and 0<=y<len(board[0]) and [x,y]!=curr[(i+1)%2] and board[x][y]==0:
+                    #print(curr,[x,y])
+                    #가로일때
+                    if curr[i][0]==curr[(i+1)%2][0]:
+                        #옆으로 이동
+                        if j[0]==0:
+                            if x==len(board)-1 and y==len(board[0])-1:
                                 answer=cnt+1
+                            else:
+                                if "".join(map(str,curr[i]))+"".join(map(str,[x,y])) not in robot:
+                                    queue.append([[curr[i],[x,y]],cnt+1])
+                        #세로로 이동
                         else:
-                            queue.append([[curr[i],[x,y]],cnt+1])
-                            visited[x][y]+=1
+                            if x==len(board)-1 and y==len(board[0])-1:
+                                answer=cnt+1
+                            else:
+                                if board[x][curr[(i+1)%2][1]]==0 and "".join(map(str,curr[i]))+"".join(map(str,[x,y])) not in robot:
+                                    queue.append([[curr[i],[x,y]],cnt+1])
+                    #세로일때
+                    else:
+                        #세로로 이동
+                        if j[1]==0:
+                            if x==len(board)-1 and y==len(board[0])-1:
+                                answer=cnt+1
+                            else:
+                                if "".join(map(str,curr[i]))+"".join(map(str,[x,y])) not in robot:
+                                    queue.append([[curr[i],[x,y]],cnt+1])
+                        #옆으로 이동
+                        else:
+                            if x==len(board)-1 and y==len(board[0])-1:
+                                answer=cnt+1
+                            else:
+                                if board[curr[(i+1)%2][0]][y]==0 and "".join(map(str,curr[i]))+"".join(map(str,[x,y])) not in robot:
+                                    queue.append([[curr[i],[x,y]],cnt+1])
+
+    #print(robot)
+
     return answer
 
-board=[[0, 0, 0, 0, 0, 0, 1], 
-       [1, 1, 1, 1, 0, 0, 1], 
-       [0, 0, 0, 0, 0, 0, 0], 
-       [0, 0, 1, 1, 1, 1, 0], 
-       [0, 1, 1, 1, 1, 1, 0], 
-       [0, 0, 0, 0, 0, 1, 1], 
-       [0, 0, 1, 0, 0, 0, 0]]
+#board=[[0, 0, 0, 1, 1],[0, 0, 0, 1, 0],[0, 1, 0, 1, 1],[1, 1, 0, 0, 1],[0, 0, 0, 0, 0]]
+board=[[0, 0, 0, 0, 1, 0], [0, 0, 1, 1, 1, 0], [0, 1, 1, 1, 1, 0], [0, 1, 0, 0, 1, 0], [0, 0, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0]]
 print(solution(board))
