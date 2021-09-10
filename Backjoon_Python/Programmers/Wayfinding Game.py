224f1b7980041dbf9e9162e017916522760f986f
khsung <khsung0@nate.com>
2021-09-10 12:46:16 +0900

    길 찾기 게임(합격)

--- Backjoon_Python/Programmers/Wayfinding Game.py
+++ Backjoon_Python/Programmers/Wayfinding Game.py
@@ -1,33 +1,55 @@
 #길 찾기 게임
+import sys
+sys.setrecursionlimit(10**6)
+def pre(tree,curr):
+    global answer
+    answer[0].append(curr)
+    if tree[curr][1]!=".":
+        pre(tree,tree[curr][1])
+    if tree[curr][2]!=".":
+        pre(tree,tree[curr][2])
+
+def post(tree,curr):
+    global answer
+    if tree[curr][1]!=".":
+        post(tree,tree[curr][1])
+    if tree[curr][2]!=".":
+        post(tree,tree[curr][2])
+    answer[1].append(curr)
+
 def solution(nodeinfo):
+    global answer
     answer = [[],[]]
-    #tree={}
-    #for i in range(len(nodeinfo)):
-    #    nodeinfo[i].append(i+1)
-    #nodeinfo.sort(key=lambda x:(-x[1],x[0]))
-    #tree[nodeinfo[0][2]]=[-1,-1]
-    #curr=[[nodeinfo[0]]]
-    #cur_parent=0
-    #if len(nodeinfo)==1:
-    #    answer=[[root],[root]]
-    #else:
-    #    for i in range(len(nodeinfo)-1):
-    #        if nodeinfo[i+1][1]<curr[cur_parent][0][1]:
-    #            cur_parent+=1
-    #            curr.append([])
-    #            curr[cur_parent].append(nodeinfo[i+1])
-    #        else:
-    #            curr[cur_parent].append(nodeinfo[i+1])
-    #        check=True
-    #        for j in range(len(curr[cur_parent-1])):
-    #            if curr[cur_parent-1][0]>nodeinfo[i+1][0]:
-    #                tree[curr[cur_parent-1][2]]=nodeinfo[i+1][2]
-                    
-    #                check=False
-    #                break
-    #        if check:
-
-
+    tree={}
+    for i in range(len(nodeinfo)):
+        nodeinfo[i].append(i+1)
+    nodeinfo.sort(key=lambda x:(-x[1]))
+    root=nodeinfo[0][2]
+                #x좌표, left, right
+    tree[root]=[nodeinfo[0][0],".","."]
+    for i in range(1,len(nodeinfo)):
+        curr=root
+        while True:
+            #left
+            if tree[curr][0]>nodeinfo[i][0]:
+                if tree[curr][1]==".":
+                    tree[curr][1]=nodeinfo[i][2]
+                    tree[nodeinfo[i][2]]=[nodeinfo[i][0],".","."]
+                    break
+                else:
+                    curr=tree[curr][1]
+            #right
+            else:
+                if tree[curr][2]==".":
+                    tree[curr][2]=nodeinfo[i][2]
+                    tree[nodeinfo[i][2]]=[nodeinfo[i][0],".","."]
+                    break
+                else:
+                    curr=tree[curr][2]
+    pre(tree,root)
+    post(tree,root)
     return answer
-nodeinfo=[[5,3],[11,5],[13,3],[3,5],[6,1],[1,3],[8,6],[7,2],[2,2]]
+
+#nodeinfo=[[5,3],[11,5],[13,3],[3,5],[6,1],[1,3],[8,6],[7,2],[2,2]]
+nodeinfo=[[1,1]]
 print(solution(nodeinfo))
