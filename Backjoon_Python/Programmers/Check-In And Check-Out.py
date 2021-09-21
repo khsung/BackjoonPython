9b17d02423b83978e1708552ca471b226d3572b5
khsung <khsung0@nate.com>
2021-09-21 22:03:34 +0900

    입실 퇴실(초기화)

--- Backjoon_Python/Programmers/Check-In And Check-Out.py
+++ Backjoon_Python/Programmers/Check-In And Check-Out.py
@@ -2,16 +2,7 @@
 def solution(enter, leave):
     answer = [0 for i in range(len(enter))]
 
-    for i in range(len(enter)):
-        visited=[0 for a in range(len(enter))]
-        for j in range(i+1,len(enter)):
-            visited[enter[j]-1]=1
-        for j in range(len(leave)):
-            if leave[j]==enter[i]:
-                break
-            elif visited[leave[j]-1]==1:
-                answer[enter[i]-1]+=1
-                answer[leave[j]-1]+=1
+    
 
     return answer
 
