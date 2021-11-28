7e2d1a1a4c0144e589b5bdc1ac4ced4c3315d903
khsung <khsung0@nate.com>
2021-11-28 16:01:48 +0900

    A번 Ресторан(합격)

---
+++ Backjoon_Python/Contest/2021 Sogang Programming Contest_A번_Ресторан.py
@@ -0,0 +1,10 @@
+#A번 Ресторан
+word=input()
+russ={"B":"v","E":"ye","H":"n","P":"r","C":"s","Y":"u","X":"h"}
+res=""
+for i in word:
+    if i in russ:
+        res+=russ[i]
+    else:
+        res+=i.lower()
+print(res)
